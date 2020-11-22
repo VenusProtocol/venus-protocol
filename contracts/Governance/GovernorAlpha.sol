d6039998143c8e0a96ed6b7ef69d5f791da6cb18
brightdev33 <brightdev33@gmail.com>
2020-11-22 16:49:20 +1000

    Updated governor threshold

--- contracts/Governance/GovernorAlpha.sol
+++ contracts/Governance/GovernorAlpha.sol
@@ -6,10 +6,10 @@ contract GovernorAlpha {
     string public constant name = "Venus Governor Alpha";
 
     /// @notice The number of votes in support of a proposal required in order for a quorum to be reached and for a vote to succeed
-    function quorumVotes() public pure returns (uint) { return 400000e18; } // 400,000 = 4% of XVS
+    function quorumVotes() public pure returns (uint) { return 600000e18; } // 600,000 = 2% of XVS
 
     /// @notice The number of votes required in order for a voter to become a proposer
-    function proposalThreshold() public pure returns (uint) { return 100000e18; } // 100,000 = 1% of XVS
+    function proposalThreshold() public pure returns (uint) { return 300000e18; } // 300,000 = 1% of XVS
 
     /// @notice The maximum number of actions that can be included in a proposal
     function proposalMaxOperations() public pure returns (uint) { return 10; } // 10 actions
