2ecadb9d40c7e6fa71fabbf636f09f074dab3552
brightdev33 <brightdev33@gmail.com>
2021-05-28 02:14:59 +1000

    Removed address 0 check

--- contracts/VToken.sol
+++ contracts/VToken.sol
@@ -1147,8 +1147,8 @@ contract VToken is VTokenInterface, Exponential, TokenErrorReporter {
       * @return uint 0=success, otherwise a failure (see ErrorReporter.sol for details)
       */
     function _acceptAdmin() external returns (uint) {
-        // Check caller is pendingAdmin and pendingAdmin ≠ address(0)
-        if (msg.sender != pendingAdmin || msg.sender == address(0)) {
+        // Check caller is pendingAdmin
+        if (msg.sender != pendingAdmin) {
             return fail(Error.UNAUTHORIZED, FailureInfo.ACCEPT_ADMIN_PENDING_ADMIN_CHECK);
         }
 
