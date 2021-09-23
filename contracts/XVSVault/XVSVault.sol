7f3fba780dce5cb9bf6b856174950c40b294913a
greenrose51 <greenrose9251@gmail.com>
2021-09-23 19:30:04 +0300

    add delegate feature to XVS Vault

--- contracts/XVSVault/XVSVault.sol
+++ contracts/XVSVault/XVSVault.sol
@@ -26,6 +26,18 @@ contract XVSVault is XVSVaultStorage {
     /// @notice Event emitted when admin changed
     event AdminTransferred(address indexed oldAdmin, address indexed newAdmin);
 
+    /// @notice An event thats emitted when an account changes its delegate
+    event DelegateChanged(address indexed delegator, address indexed fromDelegate, address indexed toDelegate);
+
+    /// @notice An event thats emitted when a delegate account's vote balance changes
+    event DelegateVotesChanged(address indexed delegate, uint previousBalance, uint newBalance);
+
+    /// @notice The standard BEP-20 transfer event
+    event Transfer(address indexed from, address indexed to, uint256 amount);
+
+    /// @notice The standard BEP-20 approval event
+    event Approval(address indexed owner, address indexed spender, uint256 amount);
+
     constructor() public {
         admin = msg.sender;
     }
@@ -144,6 +156,10 @@ contract XVSVault is XVSVaultStorage {
         );
         user.amount = user.amount.add(_amount);
         user.rewardDebt = user.amount.mul(pool.accRewardPerShare).div(1e12);
+
+        // Update Delegate Amount
+        _updateDelegate(address(msg.sender), uint96(user.amount));
+
         emit Deposit(msg.sender, _rewardToken, _pid, _amount);
     }
 
@@ -173,6 +189,9 @@ contract XVSVault is XVSVaultStorage {
         pool.token.transfer(address(msg.sender), _amount);
 
         withdrawal.amount = 0;
+
+        // Update Delegate Amount
+        _updateDelegate(address(msg.sender), uint96(user.amount));
 
         emit ExecutedWithdrawal(msg.sender, _rewardToken, _pid, _amount);
     }
@@ -315,14 +334,12 @@ contract XVSVault is XVSVaultStorage {
     }
 
     /**
-     * @notice Determine the xvs stake balance for an account
+     * @notice Get the XVS stake balance of an account
      * @param account The address of the account to check
-     * @param blockNumber The block number to get the vote balance at
      * @return The balance that user staked
      */
-    function getPriorVotes(address account, uint256 blockNumber) external view returns (uint96) {
-        require(blockNumber < block.number, "XVSVault::getPriorVotes: not yet determined");
-        require(xvsAddress != address(0), "XVSVault:getPriorVotes: xvs address is not set");
+    function getStakeAmount(address account) internal view returns (uint96) {
+        require(xvsAddress != address(0), "XVSVault::getStakeAmount: xvs address is not set");
 
         PoolInfo[] storage poolInfo = poolInfos[xvsAddress];
 
@@ -334,6 +351,167 @@ contract XVSVault is XVSVaultStorage {
             }
         }
         return uint96(0);
+    }
+
+    /**
+     * @notice Update Delegates - voting power
+     * @param delegator The address of Delegator
+     * @param amount Updated delegate amount
+     */
+    function _updateDelegate(address delegator, uint96 amount) internal {
+        address currentDelegate = delegates[delegator];
+
+        if (currentDelegate != address(0)) {
+            uint32 delegateRepNum = numCheckpoints[currentDelegate];
+            uint96 delegateRepOld = delegateRepNum > 0 ? checkpoints[currentDelegate][delegateRepNum - 1].votes : 0;
+            _writeCheckpoint(currentDelegate, delegateRepNum, delegateRepOld, amount);
+        }
+    }
+
+    /**
+     * @notice Delegate votes from `msg.sender` to `delegatee`
+     * @param delegatee The address to delegate votes to
+     */
+    function delegate(address delegatee) public {
+        return _delegate(msg.sender, delegatee);
+    }
+
+    /**
+     * @notice Delegates votes from signatory to `delegatee`
+     * @param delegatee The address to delegate votes to
+     * @param nonce The contract state required to match the signature
+     * @param expiry The time at which to expire the signature
+     * @param v The recovery byte of the signature
+     * @param r Half of the ECDSA signature pair
+     * @param s Half of the ECDSA signature pair
+     */
+    function delegateBySig(address delegatee, uint nonce, uint expiry, uint8 v, bytes32 r, bytes32 s) public {
+        bytes32 domainSeparator = keccak256(abi.encode(DOMAIN_TYPEHASH, keccak256(bytes("XVSVault")), getChainId(), address(this)));
+        bytes32 structHash = keccak256(abi.encode(DELEGATION_TYPEHASH, delegatee, nonce, expiry));
+        bytes32 digest = keccak256(abi.encodePacked("\x19\x01", domainSeparator, structHash));
+        address signatory = ecrecover(digest, v, r, s);
+        require(signatory != address(0), "XVSVault::delegateBySig: invalid signature");
+        require(nonce == nonces[signatory]++, "XVSVault::delegateBySig: invalid nonce");
+        require(now <= expiry, "XVSVault::delegateBySig: signature expired");
+        return _delegate(signatory, delegatee);
+    }
+
+    /**
+     * @notice Gets the current votes balance for `account`
+     * @param account The address to get votes balance
+     * @return The number of current votes for `account`
+     */
+    function getCurrentVotes(address account) external view returns (uint96) {
+        uint32 nCheckpoints = numCheckpoints[account];
+        return nCheckpoints > 0 ? checkpoints[account][nCheckpoints - 1].votes : 0;
+    }
+
+    function _delegate(address delegator, address delegatee) internal {
+        address currentDelegate = delegates[delegator];
+        uint96 delegatorBalance = getStakeAmount(delegator);
+        delegates[delegator] = delegatee;
+
+        emit DelegateChanged(delegator, currentDelegate, delegatee);
+
+        _moveDelegates(currentDelegate, delegatee, delegatorBalance);
+    }
+
+    function _moveDelegates(address srcRep, address dstRep, uint96 amount) internal {
+        if (srcRep != dstRep && amount > 0) {
+            if (srcRep != address(0)) {
+                uint32 srcRepNum = numCheckpoints[srcRep];
+                uint96 srcRepOld = srcRepNum > 0 ? checkpoints[srcRep][srcRepNum - 1].votes : 0;
+                uint96 srcRepNew = sub96(srcRepOld, amount, "XVSVault::_moveVotes: vote amount underflows");
+                _writeCheckpoint(srcRep, srcRepNum, srcRepOld, srcRepNew);
+            }
+
+            if (dstRep != address(0)) {
+                uint32 dstRepNum = numCheckpoints[dstRep];
+                uint96 dstRepOld = dstRepNum > 0 ? checkpoints[dstRep][dstRepNum - 1].votes : 0;
+                uint96 dstRepNew = add96(dstRepOld, amount, "XVSVault::_moveVotes: vote amount overflows");
+                _writeCheckpoint(dstRep, dstRepNum, dstRepOld, dstRepNew);
+            }
+        }
+    }
+
+    function _writeCheckpoint(address delegatee, uint32 nCheckpoints, uint96 oldVotes, uint96 newVotes) internal {
+        uint32 blockNumber = safe32(block.number, "XVSVault::_writeCheckpoint: block number exceeds 32 bits");
+
+        if (nCheckpoints > 0 && checkpoints[delegatee][nCheckpoints - 1].fromBlock == blockNumber) {
+            checkpoints[delegatee][nCheckpoints - 1].votes = newVotes;
+        } else {
+            checkpoints[delegatee][nCheckpoints] = Checkpoint(blockNumber, newVotes);
+            numCheckpoints[delegatee] = nCheckpoints + 1;
+        }
+
+        emit DelegateVotesChanged(delegatee, oldVotes, newVotes);
+    }
+
+    function safe32(uint n, string memory errorMessage) internal pure returns (uint32) {
+        require(n < 2**32, errorMessage);
+        return uint32(n);
+    }
+
+    function safe96(uint n, string memory errorMessage) internal pure returns (uint96) {
+        require(n < 2**96, errorMessage);
+        return uint96(n);
+    }
+
+    function add96(uint96 a, uint96 b, string memory errorMessage) internal pure returns (uint96) {
+        uint96 c = a + b;
+        require(c >= a, errorMessage);
+        return c;
+    }
+
+    function sub96(uint96 a, uint96 b, string memory errorMessage) internal pure returns (uint96) {
+        require(b <= a, errorMessage);
+        return a - b;
+    }
+
+    function getChainId() internal pure returns (uint) {
+        uint256 chainId;
+        assembly { chainId := chainid() }
+        return chainId;
+    }
+
+    /**
+     * @notice Determine the xvs stake balance for an account
+     * @param account The address of the account to check
+     * @param blockNumber The block number to get the vote balance at
+     * @return The balance that user staked
+     */
+    function getPriorVotes(address account, uint256 blockNumber) external view returns (uint96) {
+        require(blockNumber < block.number, "XVSVault::getPriorVotes: not yet determined");
+
+        uint32 nCheckpoints = numCheckpoints[account];
+        if (nCheckpoints == 0) {
+            return 0;
+        }
+
+        // First check most recent balance
+        if (checkpoints[account][nCheckpoints - 1].fromBlock <= blockNumber) {
+            return checkpoints[account][nCheckpoints - 1].votes;
+        }
+
+        // Next check implicit zero balance
+        if (checkpoints[account][0].fromBlock > blockNumber) {
+            return 0;
+        }
+
+        uint32 lower = 0;
+        uint32 upper = nCheckpoints - 1;
+        while (upper > lower) {
+            uint32 center = upper - (upper - lower) / 2; // ceil, avoiding overflow
+            Checkpoint memory cp = checkpoints[account][center];
+            if (cp.fromBlock == blockNumber) {
+                return cp.votes;
+            } else if (cp.fromBlock < blockNumber) {
+                lower = center;
+            } else {
+                upper = center - 1;
+            }
+        }
+        return checkpoints[account][lower].votes;
     }
 
     /**
