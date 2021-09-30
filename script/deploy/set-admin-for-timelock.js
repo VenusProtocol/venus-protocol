488f6eb972e95a52dae5fc0cb859554eebe7cfc6
lakshmi <defipool@gmail.com>
2021-09-30 17:08:55 +0800

    Accounts Object added to rinkeby.json to set configurable Admin for Timelock

--- script/deploy/set-admin-for-timelock.js
+++ script/deploy/set-admin-for-timelock.js
@@ -1,8 +1,11 @@
 const contractConfigData = require("../../networks/rinkeby.json");
+
 const {
   bnbUnsigned,
-  encodeParameters
+  encodeParameters,
+  sleep
 } = require('./utils/web3-utils');
+
 
 (async () => {
     const timelockAddress = contractConfigData.Contracts.Timelock;
@@ -14,23 +17,47 @@ const {
     const currentAdminOfTimelock = await timelockContractInstance.methods.admin().call();
     console.log(`currentAdmin of timelock is: ${currentAdminOfTimelock}`);
 
+    const newAdminOfTimelock = contractConfigData.Accounts.TimelockAdmin;
+
     //queuing timelock's admin update transaction
 
-    let value = bnbUnsigned(0);
-    const signature = 'setPendingAdmin(address)';
-    const timelockNewAdmin = contractConfigData.Contracts.GovernorAlpha;
-    const data = encodeParameters(['address'], [timelockNewAdmin]);
-    const configuredDelay = await timelockContractInstance.methods.delay().call(); 
-    delay = bnbUnsigned(configuredDelay);
-    const blockNumber = await saddle.web3.eth.getBlockNumber();
-    const block = await saddle.web3.eth.getBlock(blockNumber);
-    const timestamp = block.timestamp;
-    const eta = timestamp + delay;
-    console.log(`Timelock - eta for queuedTransaction is: ${eta}`);
+  let value = bnbUnsigned(0);
+  const signature_queueTxn = 'setPendingAdmin(address)';
+  const data_queueTxn = encodeParameters(['address'], [newAdminOfTimelock]);
 
-    await saddle.send(timelockContractInstance, 'queueTransaction', [timelockAddress, value, signature, data, eta], {
-            from: currentAdminOfTimelock
-    });
+  //compute eta
+  const configuredDelay = await timelockContractInstance.methods.delay().call(); 
+  const delay = bnbUnsigned(configuredDelay);
+  const blockNumber = await saddle.web3.eth.getBlockNumber();
+  const block = await saddle.web3.eth.getBlock(blockNumber);
+  const timestamp = block.timestamp;
+  const eta = bnbUnsigned(timestamp).add(delay).add(120);
+  console.log(`configuredDelay: ${configuredDelay} - delay: ${delay}`);
+  console.log(`before sleep: currentBlockNumber: ${blockNumber} - currentBlockTimeStamp: ${timestamp} - eta: ${eta}`);
 
-    console.log(`completed setPendingAdmin of timelock to: ${timelockNewAdmin}`);
+  //5. queueTransaction (_acceptAdmin(address))
+  await saddle.send(timelockContractInstance, 'queueTransaction', [timelockAddress, value, signature_queueTxn, data_queueTxn, eta], {
+          from: currentAdminOfTimelock
+  });
+
+  console.log(`completed queuing of setPendingAdmin(address) of Timelock to: ${newAdminOfTimelock}`);
+
+  await sleep(180000);
+
+  console.log(`slept for 180 seconds after _acceptAdmin of unitroller to: ${newAdminOfTimelock}`);
+
+  const blockNumber_AfterSleep = await saddle.web3.eth.getBlockNumber();
+  const block_AfterSleep = await saddle.web3.eth.getBlock(blockNumber_AfterSleep);
+  const timestamp_AfterSleep = block_AfterSleep.timestamp;
+  console.log(`after sleep: currentBlockNumber: ${blockNumber_AfterSleep} - currentBlockTimeStamp: ${timestamp_AfterSleep} - eta: ${eta}`);
+
+  const signature_executeTxn = 'setPendingAdmin(address)';
+  const data_executeTxn = encodeParameters(['address'], [newAdminOfTimelock]);
+
+  //6. executeTransaction (setPendingAdmin(address))
+  await saddle.send(timelockContractInstance, 'executeTransaction', [timelockAddress, value, signature_executeTxn, data_executeTxn, eta], {
+    from: currentAdminOfTimelock
+  });
+
+  console.log(`executed queued-Transaction of setPendingAdmin(address) of Timelock to: ${newAdminOfTimelock}`);
 })();
