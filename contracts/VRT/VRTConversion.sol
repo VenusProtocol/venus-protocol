a9cbfbf96657088385014e34963158c6bed1b5cf
lakshmi <defipool@gmail.com>
2021-12-06 20:21:34 +0800

    Refactor VRTConversion Contract

--- contracts/VRT/VRTConversion.sol
+++ contracts/VRT/VRTConversion.sol
@@ -2,17 +2,47 @@ pragma solidity ^0.5.16;
 
 import "../Utils/IBEP20.sol";
 import "../Utils/SafeBEP20.sol";
-import "../Ownable.sol";
-import "./VRTConversionProxy.sol";
-import "./VRTConversionStorage.sol";
 
 /**
  * @title Venus's VRTConversion Contract
  * @author Venus
  */
-contract VRTConversion is VRTConversionV1Storage {
+contract VRTConversion {
     using SafeMath for uint256;
     using SafeBEP20 for IBEP20;
+
+    /// @notice Administrator for this contract
+    address public admin;
+
+    /// @notice Pending administrator for this contract
+    address public pendingAdmin;
+
+    /// @notice Guard variable for re-entrancy checks
+    bool internal _notEntered;
+
+    /// @notice VRTToken Address
+    address public vrtAddress;
+
+    /// @notice decimal precision for VRT
+    uint256 public vrtDecimalsMultiplier = 10**18;
+
+    /// @notice XVSToken address
+    address public xvsAddress;
+
+    /// @notice decimal precision for XVS
+    uint256 public xvsDecimalsMultiplier = 10**18;
+
+    /// @notice Conversion ratio from VRT to XVS with decimal 18
+    uint256 public conversionRatio;
+
+    /// @notice timestamp from which VRT to XVS is allowed
+    uint256 public conversionStartTime;
+
+    /// @notice Emitted when pendingAdmin is accepted, which means admin is updated
+    event NewAdmin(address oldAdmin, address newAdmin);
+
+    /// @notice Emitted when pendingAdmin is changed
+    event NewPendingAdmin(address oldPendingAdmin, address newPendingAdmin);
 
     /// @notice Emitted when an admin set convrsion info
     event ConversionInfoSet(
@@ -34,10 +64,9 @@ contract VRTConversion is VRTConversionV1Storage {
 
     constructor(address _vrtAddress, address _xvsAddress) public {
         admin = msg.sender;
-        vrtAddresses = _xvsAddress;
+        vrtAddress = _xvsAddress;
         xvsAddress = _xvsAddress;
-        vrtDecimalsMultiplier = 10**(uint256(IBEP20(vrtAddresses).decimals()));
-        xvsDecimalsMultiplier = 10**(uint256(IBEP20(xvsAddress).decimals()));
+        _notEntered = true;
     }
 
     modifier onlyAdmin() {
@@ -46,8 +75,49 @@ contract VRTConversion is VRTConversionV1Storage {
     }
 
     /**
-     * @notice Transfer tokenA and redeem tokenB
-     * @dev Note: If there is not enough tokenB, we do not perform the conversion.
+     * @notice Begins transfer of admin rights. The newPendingAdmin must call `_acceptAdmin` to finalize the transfer.
+     * @dev Admin function to begin change of admin. The newPendingAdmin must call `_acceptAdmin` to finalize the transfer.
+     * @param newPendingAdmin New pending admin.
+     */
+    function _setPendingAdmin(address newPendingAdmin) external {
+        // Check caller = admin
+        require(msg.sender == admin, "SET_PENDING_ADMIN_OWNER_CHECK");
+
+        // Save current value, if any, for inclusion in log
+        address oldPendingAdmin = pendingAdmin;
+
+        // Store pendingAdmin with value newPendingAdmin
+        pendingAdmin = newPendingAdmin;
+
+        // Emit NewPendingAdmin(oldPendingAdmin, newPendingAdmin)
+        emit NewPendingAdmin(oldPendingAdmin, newPendingAdmin);
+    }
+
+    /**
+     * @notice Accepts transfer of admin rights. msg.sender must be pendingAdmin
+     * @dev Admin function for pending admin to accept role and update admin
+     */
+    function _acceptAdmin() external {
+        // Check caller is pendingAdmin
+        require(msg.sender == pendingAdmin, "ACCEPT_ADMIN_PENDING_ADMIN_CHECK");
+
+        // Save current values for inclusion in log
+        address oldAdmin = admin;
+        address oldPendingAdmin = pendingAdmin;
+
+        // Store admin with value pendingAdmin
+        admin = pendingAdmin;
+
+        // Clear the pending value
+        pendingAdmin = address(0);
+
+        emit NewAdmin(oldAdmin, admin);
+        emit NewPendingAdmin(oldPendingAdmin, pendingAdmin);
+    }
+
+    /**
+     * @notice Transfer VRT and redeem XVS
+     * @dev Note: If there is not enough XVS, we do not perform the conversion.
      * @param vrtAmount The amount of VRT
      * @return The amount of XVS which is converted
      */
@@ -56,20 +126,14 @@ contract VRTConversion is VRTConversionV1Storage {
         nonReentrant
         returns (uint256)
     {
+        require(vrtAmount > 0, "VRT amount must be non-zero");
         require(conversionRatio > 0, "conversion ratio is incorrect");
         require(
             conversionStartTime > block.timestamp,
             "conversions didn't start yet"
         );
 
-        uint256 beforeAmount = IBEP20(vrtAddresses).balanceOf(address(this));
-        IBEP20(vrtAddresses).transferFrom(msg.sender, address(this), vrtAmount);
-        uint256 afterAmount = IBEP20(vrtAddresses).balanceOf(address(this));
-
-        uint256 actualAmount = afterAmount.sub(beforeAmount);
-        require(actualAmount > 0, "token A transfer failed");
-
-        uint256 redeemAmount = actualAmount
+        uint256 redeemAmount = vrtAmount
             .mul(conversionRatio)
             .mul(xvsDecimalsMultiplier)
             .div(1e18)
@@ -79,31 +143,22 @@ contract VRTConversion is VRTConversionV1Storage {
             "not enough XVSTokens"
         );
 
-        IBEP20(xvsAddress).safeTransfer(msg.sender, redeemAmount);
-
         emit TokenConverted(
             msg.sender,
-            vrtAddresses,
+            vrtAddress,
             xvsAddress,
-            actualAmount,
+            vrtAmount,
             redeemAmount
         );
-        return redeemAmount;
-    }
 
-    /*** VRTConversion Admin Functions ***/
-
-    function _become(VRTConversionProxy proxy) external {
-        require(
-            msg.sender == proxy.admin(),
-            "only proxy admin can change brains"
+        IBEP20(vrtAddress).safeTransferFrom(
+            msg.sender,
+            address(this),
+            vrtAmount
         );
-        require(proxy._acceptImplementation() == 0, "change not authorized");
-    }
+        IBEP20(xvsAddress).safeTransfer(msg.sender, redeemAmount);
 
-    function initialize() public onlyAdmin {
-        // The counter starts true to prevent changing it from zero to non-zero (i.e. smaller cost/refund)
-        _notEntered = true;
+        return redeemAmount;
     }
 
     /**
@@ -112,7 +167,7 @@ contract VRTConversion is VRTConversionV1Storage {
      * @param _conversionStartTime The conversion available cycle with timestamp
      */
     function _setXVSVRTConversionInfo(
-        uint _conversionRatio,
+        uint256 _conversionRatio,
         uint256 _conversionStartTime
     ) public onlyAdmin {
         conversionRatio = _conversionRatio;
