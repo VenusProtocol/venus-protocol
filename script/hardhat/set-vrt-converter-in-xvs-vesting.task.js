c5fd50e9a4b4e4daf886403f4cc7c8d77d4d0c81
lakshmi <defipool@gmail.com>
2022-01-26 14:05:25 +0800

    updated VRTConverter deployment script and set VRTConverterAddress in XVSVesting

---
+++ script/hardhat/set-vrt-converter-in-xvs-vesting.task.js
@@ -0,0 +1,22 @@
+const { task, types } = require("hardhat/config");
+require("@nomiclabs/hardhat-ethers");
+require("dotenv").config();
+const contractConfigData = require(`../../networks/testnet.json`);
+
+task("set-vrt-converter-in-xvs-vesting", "sets VRTConverter Address in XVSVesting contract")
+    .setAction(async (args, hre) => {
+        const _xvsVestingAddress = contractConfigData.Contracts.XVSVesting;
+
+        //load XVSVesting Instance
+        const xvsVestingContractInstance = await hre.ethers.getContractAt(
+            "XVSVesting",
+            _xvsVestingAddress
+        );
+
+        const _vrtConverterAddress = contractConfigData.Contracts.VRTConverter;
+
+        //set VRTConverter-Address in XVSVesting
+        const txResponse = await xvsVestingContractInstance._setVrtConversion(_vrtConverterAddress);
+        await txResponse.wait();
+        console.log(`txResponse to set VRTConverter-Address in XVSVesting is: ${JSON.stringify(txResponse)}`);
+    });
