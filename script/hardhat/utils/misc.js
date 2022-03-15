0884cf605981d2b5761a6837b6adaf1b9a43bda5
sam.h <>
2022-03-16 04:00:48 +0800

    add simulation script for withdrawing fund from treasury

--- script/hardhat/utils/misc.js
+++ script/hardhat/utils/misc.js
@@ -11,19 +11,24 @@ function deploy(contract, ...arguments) {
     return Contract.deploy({ data: obj.bytecode, arguments })
 }
 
+const IMPERSONATION_STARTING_BALANCE = '0x10000000000000000000000';
+
+function setBalance(addr, balance) {
+  return network.provider.send("hardhat_setBalance", [addr, balance]);
+}
+
 async function impersonate(addr) {
     await network.provider.request({
         method: "hardhat_impersonateAccount",
         params: [addr],
     });
-    await network.provider.send("hardhat_setBalance", [
-        addr,
-        "0x10000000000000000000000",
-    ]);
+    await setBalance(addr, IMPERSONATION_STARTING_BALANCE);
 }
 
 module.exports = {
     deploy,
     getContractAt,
-    impersonate
+    impersonate,
+    setBalance,
+    IMPERSONATION_STARTING_BALANCE,
 }
