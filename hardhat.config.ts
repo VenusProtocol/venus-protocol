d2b401c85d68c6ead3e621002e7472e2a32f6a73
Kirill Kuvshinov <kkirka@gmail.com>
2022-08-10 18:16:44 +0300

    Fix indentation in hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,67 +1,67 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
- import { HardhatUserConfig } from "hardhat/types";
- import '@openzeppelin/hardhat-upgrades';
- 
- import "@nomiclabs/hardhat-truffle5";
- import "@nomiclabs/hardhat-waffle";
- import "@nomiclabs/hardhat-etherscan";
- 
- import { ethers } from 'ethers';
- require("dotenv").config();
- 
- const BSCSCAN_API_KEY = process.env.BSCSCAN_API_KEY;
- const DEPLOYER_PRIVATE_KEY = process.env.DEPLOYER_PRIVATE_KEY;
- 
- const config: HardhatUserConfig = {
-   defaultNetwork: "hardhat",
-   solidity: {
-     compilers: [
-       {
-         version: "0.5.16",
-         settings: {
-           optimizer: {
-             enabled: true,
-             runs: 200
-           }
-         }
-       }
-     ],
-   },
-   networks: {
-     bsctestnet: {
-       url: process.env.BSC_TESTNET_NODE || 'https://data-seed-prebsc-1-s1.binance.org:8545',
-       chainId: 97,
-       accounts: DEPLOYER_PRIVATE_KEY ? [`0x${DEPLOYER_PRIVATE_KEY}`] : [],
-       gasPrice: ethers.utils.parseUnits("10", "gwei").toNumber(),
-       gasMultiplier: 10,
-       timeout: 12000000,
-     },
-     hardhat: {
-       chainId: 56,
-       forking: {
-         url: process.env.BSC_ARCHIVE_NODE || '',
-       }
-     },
-     // currently not used, we are still using saddle to deploy contracts
-     bscmainnet: {
-       url: `https://bsc-dataseed.binance.org/`,
-       accounts: DEPLOYER_PRIVATE_KEY ? [`0x${DEPLOYER_PRIVATE_KEY}`] : []
-     },
-   },
-   etherscan: {
-     apiKey: BSCSCAN_API_KEY,
-   },
-   paths: {
-     sources: "./contracts",
-     tests: "./tests/hardhat",
-     cache: "./cache",
-     artifacts: "./artifacts"
-   },
-   mocha: {
-     timeout: 20000
-   }
- };
- 
- export default config;
+import { HardhatUserConfig } from "hardhat/types";
+import '@openzeppelin/hardhat-upgrades';
+
+import "@nomiclabs/hardhat-truffle5";
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-etherscan";
+
+import { ethers } from 'ethers';
+require("dotenv").config();
+
+const BSCSCAN_API_KEY = process.env.BSCSCAN_API_KEY;
+const DEPLOYER_PRIVATE_KEY = process.env.DEPLOYER_PRIVATE_KEY;
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  solidity: {
+    compilers: [
+      {
+        version: "0.5.16",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
+      }
+    ],
+  },
+  networks: {
+    bsctestnet: {
+      url: process.env.BSC_TESTNET_NODE || 'https://data-seed-prebsc-1-s1.binance.org:8545',
+      chainId: 97,
+      accounts: DEPLOYER_PRIVATE_KEY ? [`0x${DEPLOYER_PRIVATE_KEY}`] : [],
+      gasPrice: ethers.utils.parseUnits("10", "gwei").toNumber(),
+      gasMultiplier: 10,
+      timeout: 12000000,
+    },
+    hardhat: {
+      chainId: 56,
+      forking: {
+        url: process.env.BSC_ARCHIVE_NODE || '',
+      }
+    },
+    // currently not used, we are still using saddle to deploy contracts
+    bscmainnet: {
+      url: `https://bsc-dataseed.binance.org/`,
+      accounts: DEPLOYER_PRIVATE_KEY ? [`0x${DEPLOYER_PRIVATE_KEY}`] : []
+    },
+  },
+  etherscan: {
+    apiKey: BSCSCAN_API_KEY,
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./tests/hardhat",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 20000
+  }
+};
+
+export default config;
