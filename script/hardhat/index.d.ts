3918e85f1f00c5417c4817e9108f8c5dc79ff71e
Corey Rice <coreyarice@gmail.com>
2022-08-30 17:29:16 -0300

    Add index file

---
+++ script/hardhat/index.d.ts
@@ -0,0 +1,5 @@
+// Governance
+export function deployGovernorBravoDelegate(): string { }
+export function deployGovernorBravoDelegator(config: { timelockAddress: string, xvsVaultAddress: string, guardianAddress: string, governorBravoDelegateAddress: string }) { }
+export function verifyGovernorBravoDelegate() { }
+export function verifyGovernorBravoDelegator() { }
---
+++ script/hardhat/index.js
@@ -0,0 +1,4 @@
+exports.deployGovernorBravoDelegate = require('./governance/deploy-governor-bravo-delegate')
+exports.deployGovernorBravoDelegator = require('./governance/deploy-governor-bravo-delegator')
+exports.verifyGovernorBravoDelegate = require('./governance/verify-governor-bravo-delegate')
+exports.verifyGovernorBravoDelegator = require('./governance/verify-governor-bravo-delegator')
