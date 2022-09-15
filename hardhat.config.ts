18ef3dabe9b2626782d0ac534d740ff31b16422d
Corey Rice <coreyarice@gmail.com>
2022-09-15 09:23:34 -0300

    install isolated pools from package

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -112,7 +112,7 @@ const config: HardhatUserConfig = {
   external: {
     contracts: [
       {
-        artifacts: "node_modules/isolated-pools/artifacts",
+        artifacts: "node_modules/@venusprotocol/isolated-pools/artifacts",
       }
     ],
   }
