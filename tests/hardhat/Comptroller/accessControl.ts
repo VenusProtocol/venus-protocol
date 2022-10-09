b7edb738b18c0a6e1bdbce15e1ecb2405a0d9ad3
0xLucian <0xluciandev@gmail.com>
2022-10-09 23:04:34 +0300

    add all tests cases (unimplemented)

--- tests/hardhat/Comptroller/accessControl.ts
+++ tests/hardhat/Comptroller/accessControl.ts
@@ -1,14 +1,45 @@
-import {  Signer } from "ethers";
+import { Signer } from "ethers";
 import { ethers } from "hardhat";
-import {loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { smock, MockContract, FakeContract } from "@defi-wonderland/smock";
 import chai from "chai";
+import { Comptroller, Comptroller__factory, IAccessControlManager } from "../../../typechain";
 const { expect } = chai;
 chai.use(smock.matchers);
 
 describe("Comptroller", () => {
-	// let accessControlManager: FakeContract<AccessControlmanager>
-	// describe("Access Control",() => {
+  let comptroller: MockContract<Comptroller>;
+  let accessControl: FakeContract<IAccessControlManager>;
+    
+  before(async () => {
+	const ComptrollerFactory = await smock.mock<Comptroller__factory>("Comptroller");
+	comptroller = await ComptrollerFactory.deploy();
+	accessControl = await smock.fake<IAccessControlManager>("AccessControlManager");
+	await comptroller._setAccessControl(accessControl.address);
+  });
+	
+  describe("Access Control", () => {
+	it.only("ACM address is set in storage", async () => {
+		expect(await comptroller.getVariable("accessControl")).to.equal(accessControl.address);
+	});
+	
+	describe("setCollateralFactor", async () => {
 
-	// })
+	});
+	describe("setLiquidationIncentive", async () => {
+
+	});
+	describe("_setMarketBorrowCaps", async () => {
+
+	});
+	describe("setMarketSupplyCaps", async () => {
+
+	});
+	describe("setProtocolPaused", async () => {
+
+	});
+	describe("_setLiquidationIncentive", async () =>{
+
+	});
+  });
 });
