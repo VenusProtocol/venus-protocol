b5a6b085b342bc34862caa0daeaff2229a9b4a33
Narayan Prusty <narayan@Narayans-MacBook-Pro.local>
2022-11-14 23:29:22 +0000

    remove struct

--- contracts/VAIController.sol
+++ contracts/VAIController.sol
@@ -12,24 +12,6 @@ import "./ComptrollerStorage.sol";
 import "hardhat/console.sol";
 
 interface ComptrollerImplInterface {
-    struct Market {
-        /// @notice Whether or not this market is listed
-        bool isListed;
-
-        /**
-         * @notice Multiplier representing the most one can borrow against their collateral in this market.
-         *  For instance, 0.9 to allow borrowing 90% of collateral value.
-         *  Must be between 0 and 1, and stored as a mantissa.
-         */
-        uint collateralFactorMantissa;
-
-        /// @notice Per-market mapping of "accounts in this asset"
-        mapping(address => bool) accountMembership;
-
-        /// @notice Whether or not this market receives XVS
-        bool isVenus;
-    }
-
     function protocolPaused() external view returns (bool);
     function mintedVAIs(address account) external view returns (uint);
     function vaiMintRate() external view returns (uint);
