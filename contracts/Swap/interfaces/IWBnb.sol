d25c6f56fd522e34046a8fd90fc641e60973f4f0
0xLucian <0xluciandev@gmail.com>
2022-12-06 11:35:01 +0200

    inherit IERC20 interface in IWbnb

--- contracts/Swap/interfaces/IWBnb.sol
+++ contracts/Swap/interfaces/IWBnb.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: BSD-3-Clause
 pragma solidity 0.8.17;
 
-interface IWBnb {
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+interface IWBnb is IERC20 {
     function deposit() external payable;
     function withdraw(uint wad) external;
 }
