e75baf8d1e56d076688f6b6c01735cff3879a17e
narayanprusty <arsenal.narayan@gmail.com>
2023-01-03 23:08:44 +0000

    set qvl caps

--- contracts/Tokens/Prime/Prime.sol
+++ contracts/Tokens/Prime/Prime.sol
@@ -20,6 +20,53 @@ contract Prime is Ownable2StepUpgradeable, PrimeStorageV1 {
         __Ownable2Step_init();
     }
 
+    function setLimit(
+        uint256 irrevocableLimit,
+        uint256 revocableLimit
+    ) onlyOwner external {
+        require(_totalRevocable >= _revocableLimit, "limit is lower than total minted tokens");
+        require(_totalIrrevocable >= irrevocableLimit, "limit is lower than total minted tokens");
+
+        _revocableLimit = revocableLimit;
+        _irrevocableLimit = irrevocableLimit;
+    }
+
+    function setCaps(
+        uint256[] memory thresholds,
+        uint256[] memory supplyTVLCaps,
+        uint256[] memory borrowTVLCaps        
+    ) onlyOwner external {
+        require(
+            thresholds.length == 5 &&
+            supplyTVLCaps.length == 5 &&
+            borrowTVLCaps.length == 5, 
+            "you need to set caps for all tiers"
+        );
+
+        uint256 threshold;
+        uint256 borrowTVLCap;
+        uint256 supplyTVLCap;
+
+        for(uint i = 0; i < thresholds.length; i++) {
+            require(
+                thresholds[i] > threshold &&
+                borrowTVLCaps[i] > borrowTVLCap &&
+                supplyTVLCaps[i] > supplyTVLCap, 
+                "higher tier should have higher cap"
+            );
+
+            threshold = thresholds[i];
+            borrowTVLCap = borrowTVLCaps[i];
+            supplyTVLCap = supplyTVLCaps[i];
+
+            _tiers[Tier(i+1)] = Cap(
+                thresholds[i],
+                supplyTVLCaps[i],
+                borrowTVLCaps[i]
+            );
+        }
+    }
+
     function issue(
         address[] memory owners
     ) onlyOwner external {
