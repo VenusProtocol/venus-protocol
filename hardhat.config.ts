96a01ed6c1e4128b2af750f7632546ed11a932a6
0xLucian <0xluciandev@gmail.com>
2023-02-01 15:14:56 +0200

    remove unnecessary comment and bump version

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -16,8 +16,6 @@ require("dotenv").config();
 const BSCSCAN_API_KEY = process.env.BSCSCAN_API_KEY;
 const DEPLOYER_PRIVATE_KEY = process.env.DEPLOYER_PRIVATE_KEY;
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
