147c0a664607c1ae4b52c840d6db1691cbd612ab
Corey Rice <coreyarice@gmail.com>
2023-02-03 10:05:32 -0300

    refactor: update doc gen templates

--- docgen-templates/helpers.ts
+++ docgen-templates/helpers.ts
@@ -1,3 +1,14 @@
-export function or(...args) {
-  return Array.prototype.slice.call(args, 0, -1).some(Boolean);
-}
+export const and = (...args) => {
+  return args.every(Boolean);
+};
+
+export const bullet = (docString: string) =>
+  docString
+    .split("\n")
+    .map((elem: string) => {
+      if (elem) {
+        return `* ${elem}`;
+      }
+      return elem;
+    })
+    .join("\n");
