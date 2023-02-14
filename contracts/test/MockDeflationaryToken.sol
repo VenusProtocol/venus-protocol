e667d373bee72cd984e673ee11b933401abc488c
defcon022 <manmeetparmar22@gmail.com>
2023-02-14 16:22:34 +0530

    Lint fix.

--- contracts/test/MockDeflationaryToken.sol
+++ contracts/test/MockDeflationaryToken.sol
@@ -75,7 +75,6 @@ contract DeflatingERC20 {
 
     function transferFrom(address from, address to, uint value) external returns (bool) {
         if (allowance[from][msg.sender] != uint(-1)) {
-
             allowance[from][msg.sender] = allowance[from][msg.sender].sub(value);
         }
         _transfer(from, to, value);
