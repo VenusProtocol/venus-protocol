cd563c281d59cd9ec6ce19ec26ecec5c8559e66e
defcon022 <manmeetparmar22@gmail.com>
2023-03-03 12:28:23 +0530

    Added Helper libraries

---
+++ contracts/Diamond/libraries/LibAccessCheck.sol
@@ -0,0 +1,59 @@
+pragma solidity ^0.5.16;
+
+import "../Governance/IAccessControlManager.sol";
+import "./ComptrollerStorage.sol";
+
+library LibAccessCheck  {
+    /// @notice Reverts if the protocol is paused
+    function checkProtocolPauseState() internal view {
+        require(!protocolPaused, "protocol is paused");
+    }
+
+    /// @notice Reverts if a certain action is paused on a market
+    function checkActionPauseState(address market, Action action) internal view {
+        require(!actionPaused(market, action), "action is paused");
+    }
+
+    /// @notice Reverts if the caller is not admin
+    function ensureAdmin() internal view {
+        require(msg.sender == admin, "only admin can");
+    }
+
+    /// @notice Checks the passed address is nonzero
+    function ensureNonzeroAddress(address someone) internal pure {
+        require(someone != address(0), "can't be zero address");
+    }
+
+    /// @notice Reverts if the market is not listed
+    function ensureListed(Market storage market) internal view {
+        require(market.isListed, "market not listed");
+    }
+
+    /// @notice Reverts if the caller is neither admin nor the passed address
+    function ensureAdminOr(address privilegedAddress) internal view {
+        require(msg.sender == admin || msg.sender == privilegedAddress, "access denied");
+    }
+
+    function ensureAllowed(string memory functionSig) internal view {
+        require(IAccessControlManager(accessControl).isAllowedToCall(msg.sender, functionSig), "access denied");
+    }
+
+    /**
+     * @notice Returns whether the given account is entered in the given asset
+     * @param account The address of the account to check
+     * @param vToken The vToken to check
+     * @return True if the account is in the asset, otherwise false.
+     */
+    function checkMembership(address account, VToken vToken) external view returns (bool) {
+        return markets[address(vToken)].accountMembership[account];
+    }
+
+    /**
+     * @notice Checks if a certain action is paused on a market
+     * @param action Action id
+     * @param market vToken address
+     */
+    function actionPaused(address market, Action action) public view returns (bool) {
+        return _actionPaused[market][uint(action)];
+    }
+}
