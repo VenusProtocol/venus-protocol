b91f71a38d1a8c2588859a8c9d29beb258010c69
defcon022 <manmeetparmar22@gmail.com>
2023-03-17 20:35:47 +0530

    Fixed comptroller address.

--- deploy/005-deploy-swaprouter.ts
+++ deploy/005-deploy-swaprouter.ts
@@ -5,12 +5,10 @@ const ADDRESSES = {
   bsctestnet: {
     WBNBAddress: "0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd",
     pancakeFactory: "0x182859893230dC89b114d6e2D547BFFE30474a21",
-    comptrollerAddress: "0x94d1820b2D1c7c7452A163983Dc888CEC546b77D",
   },
   bscmainnet: {
     WBNBAddress: "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c",
     pancakeFactory: "0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73",
-    comptrollerAddress: "0xfD36E2c2a6789Db23113685031d7F16329158384",
   },
 };
 
@@ -22,7 +20,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const networkName = network.name === "bscmainnet" ? "bscmainnet" : "bsctestnet";
   const WBNBAddress = ADDRESSES[networkName].WBNBAddress;
   const pancakeFactoryAddress = ADDRESSES[networkName].pancakeFactory;
-  const comptrollerAddress = ADDRESSES[networkName].comptrollerAddress;
+  const comptroller = await deployments.get("Comptroller");
 
   await deploy("SwapRouter", {
     contract: "SwapRouter",
@@ -35,7 +33,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
       proxyContract: "OpenZeppelinTransparentProxy",
       execute: {
         methodName: "initialize",
-        args: [comptrollerAddress],
+        args: [comptroller.address],
       },
     },
   });
