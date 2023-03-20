68a6f80f138eb6b3f5defc610a11a920acc0db76
Aditya Rout <adi.b20612@gmail.com>
2023-03-20 12:00:48 +0530

    test:[VEN-1224] add vip-103 simulations

--- script/hardhat/fork/vip-103.ts
+++ script/hardhat/fork/vip-103.ts
@@ -1,48 +1,68 @@
 import { expect } from "chai";
+import { parseUnits } from "ethers/lib/utils";
 import { ethers } from "hardhat";
 
-import { Comptroller } from "../../../typechain";
-import { forking, testVip } from "./vip-framework";
+import { forking, pretendExecutingVip, testVip } from "./vip-framework";
 import { ProposalType } from "./vip-framework/types";
 import { makeProposal } from "./vip-framework/utils";
 
 const COMPTROLLER = "0xfd36e2c2a6789db23113685031d7f16329158384";
-const vBUSD = "0x95c78222B3D6e262426483D42CfA53685A67Ab9D";
+const NEW_VTRX = "0xC5D3466aA484B040eE977073fcF337f2c00071c1";
+const OLD_VTRX = "0x61eDcFe8Dd6bA3c891CB9bEc2dc7657B3B422E93";
+const VSXP = "0x2fF3d0F6990a40261c66E1ff2017aCBc282EB6d0";
+const VUSDC = "0xecA88125a5ADbe82614ffC12D0DB554E2e2867C8";
+const VUSDT = "0xfD5840Cd36d94D7229439859C0112a4185BC0255";
 
-const Actions = {
-  BORROW: 2,
-  ENTER_MARKETS: 7,
-};
-
-const vip103 = () => {
+export const vip103 = () => {
   const meta = {
     version: "v2",
-    title: "VIP-103 Pause BUSD Market",
+    title: "VIP-103 Gauntlet Rrecommendations",
     description: `
-    we'll follow Gauntlet recommendations related to BUSD. But, internally we consider these proposal too risky so we decided to start executing only the following actions:
+    TRXOLD. Conservative: Decrease CF to 0.30
+    TRX. Aggressive:
+      Increase CF to 0.40
+      Increase Borrow Cap to 4,000,000
+      Increase Supply Cap to 5,000,000
+    SXP. Lower SXP’s collateral factor to 0.25
+    USDC. Increase borrow cap to 124,700,000
+    USDT. Increase borrow cap to 245,500,000
 
-    Pause borrows and entering the market in vBUSD
-    Stop rewarding the market borrowers with XVS
-    via Normal VIP
-    
-    Comptroller._setActionsPaused(...)
     `,
-    forDescription: "I agree that Venus Protocol should proceed with the BUSD market pause",
-    againstDescription: "I do not think that Venus Protocol should proceed with the BUSD market pause",
-    abstainDescription: "I am indifferent to whether Venus Protocol proceeds with the BUSD market pause or not",
+    forDescription: "I agree that Venus Protocol should proceed with the Gauntlet Rrecommendations",
+    againstDescription: "I do not think that Venus Protocol should proceed with the Gauntlet Rrecommendations",
+    abstainDescription: "I am indifferent to whether Venus Protocol proceeds with the Gauntlet Rrecommendations or not",
   };
 
   return makeProposal(
     [
       {
         target: COMPTROLLER,
-        signature: "_setActionsPaused(address[],uint8[],bool)",
-        params: [[vBUSD], [Actions.BORROW, Actions.ENTER_MARKETS], true],
+        signature: "_setCollateralFactor(address,uint256)",
+        params: [OLD_VTRX, parseUnits("0.3", 18)],
+      },
+
+      {
+        target: COMPTROLLER,
+        signature: "_setCollateralFactor(address,uint256)",
+        params: [NEW_VTRX, parseUnits("0.4", 18)],
       },
       {
         target: COMPTROLLER,
-        signature: "_setVenusSpeeds(address[],uint256[],uint256[])",
-        params: [[vBUSD], ["2712673611111111"], ["0"]],
+        signature: "_setMarketBorrowCaps(address[],uint256[])",
+        params: [
+          [NEW_VTRX, VUSDC, VUSDT],
+          ["4000000000000", "124700000000000000000000000", "245500000000000000000000000"],
+        ],
+      },
+      {
+        target: COMPTROLLER,
+        signature: "_setMarketSupplyCaps(address[],uint256[])",
+        params: [[NEW_VTRX], ["5000000000000"]],
+      },
+      {
+        target: COMPTROLLER,
+        signature: "_setCollateralFactor(address,uint256)",
+        params: [VSXP, parseUnits("0.25", 18)],
       },
     ],
     meta,
@@ -50,51 +70,51 @@ const vip103 = () => {
   );
 };
 
-forking(26305917, () => {
-  let comptroller: Comptroller;
+forking(26544741, () => {});
+
+forking(26544741, () => {
+  let comptroller: any;
 
   before(async () => {
+    testVip("VIP-103 Gauntlet Rrecommendations", vip103());
     comptroller = await ethers.getContractAt("Comptroller", COMPTROLLER);
+    // await pretendExecutingVip(vip103());
   });
-
-  describe("Pre-VIP behavior", async () => {
-    it("BUSD borrowing", async () => {
-      const borrowingPaused = await comptroller.actionPaused(vBUSD, Actions.BORROW);
-      expect(borrowingPaused).to.equal(false);
-    });
-
-    it("entering BUSD market", async () => {
-      const enteringPaused = await comptroller.actionPaused(vBUSD, Actions.ENTER_MARKETS);
-      expect(enteringPaused).to.equal(false);
-    });
-
-    it("Venus Speeds", async () => {
-      const borrowSpeed = await comptroller.venusBorrowSpeeds(vBUSD);
-      const supplySpeed = await comptroller.venusSupplySpeeds(vBUSD);
-
-      expect(borrowSpeed).not.equals(0);
-      expect(supplySpeed).to.equal("2712673611111111");
-    });
-  });
-
-  testVip("VIP-103 Pause BUSD Market", vip103());
 
   describe("Post-VIP behavior", async () => {
-    it("pauses BUSD borrowing", async () => {
-      const borrowingPaused = await comptroller.actionPaused(vBUSD, Actions.BORROW);
-      expect(borrowingPaused).to.equal(true);
+    it("Decrease TRX_OLD collateral factor to 30%", async () => {
+      const market = await comptroller.markets(OLD_VTRX);
+      expect(market.collateralFactorMantissa).to.equal(parseUnits("0.3", 18));
     });
 
-    it("pauses entering BUSD market", async () => {
-      const enteringPaused = await comptroller.actionPaused(vBUSD, Actions.ENTER_MARKETS);
-      expect(enteringPaused).to.equal(true);
+    it("Increase NEW_VTRX collateral factor to 40%", async () => {
+      const market = await comptroller.markets(NEW_VTRX);
+      expect(market.collateralFactorMantissa).to.equal(parseUnits("0.4", 18));
     });
 
-    it("Venus Speeds", async () => {
-      const borrowSpeed = await comptroller.venusBorrowSpeeds(vBUSD);
-      const supplySpeed = await comptroller.venusSupplySpeeds(vBUSD);
-      expect(borrowSpeed).to.equal(0);
-      expect(supplySpeed).to.equal("2712673611111111");
+    it("Decrease SXP collateral factor to 25%", async () => {
+      const market = await comptroller.markets(VSXP);
+      expect(market.collateralFactorMantissa).to.equal(parseUnits("0.25", 18));
+    });
+
+    it("sets the borrow cap to 4,000,000 NEW_TRX", async () => {
+      const newCap = await comptroller.borrowCaps(NEW_VTRX);
+      expect(newCap).to.equal(parseUnits("4000000", 6));
+    });
+
+    it("sets the borrow cap to 124,700,000 USDC", async () => {
+      const newCap = await comptroller.borrowCaps(VUSDC);
+      expect(newCap).to.equal(parseUnits("124700000", 18));
+    });
+
+    it("sets the borrow cap to 245,500,000 USDT", async () => {
+      const newCap = await comptroller.borrowCaps(VUSDT);
+      expect(newCap).to.equal(parseUnits("245500000", 18));
+    });
+
+    it("sets the supply cap to 5,000,000 NEW_TRX", async () => {
+      const newCap = await comptroller.supplyCaps(NEW_VTRX);
+      expect(newCap).to.equal(parseUnits("5000000", 6));
     });
   });
 });
