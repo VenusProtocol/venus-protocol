e8cc370cdbb32ea267bc5167f5516209674dd0fc
aditya-routv <aditya@venus.io>
2023-03-30 14:48:04 +0530

    test: SXP collateral factor

--- script/hardhat/fork/vip-104.ts
+++ script/hardhat/fork/vip-104.ts
@@ -11,6 +11,7 @@ const COMPTROLLER = "0xfd36e2c2a6789db23113685031d7f16329158384";
 const NEW_VTRX = "0xC5D3466aA484B040eE977073fcF337f2c00071c1";
 const OLD_VTRX = "0x61eDcFe8Dd6bA3c891CB9bEc2dc7657B3B422E93";
 const VDAI = "0x334b3eCB4DCa3593BCCC3c7EBD1A1C1d1780FBF1";
+const VSXP = "0x2fF3d0F6990a40261c66E1ff2017aCBc282EB6d0";
 
 export const vip104 = () => {
   const meta = {
@@ -18,6 +19,7 @@ export const vip104 = () => {
     title: "VIP-104 Gauntlet Recommendations",
     description: `
     TRXOLD: Decrease CF to 0.20 (conservative)
+    SXP: lower collateral factor to 17.5%
     TRX: (aggressive)
     * Increase CF to 0.45
     * Increase Borrow Cap to 8,000,000
@@ -57,6 +59,12 @@ export const vip104 = () => {
         signature: "_setMarketSupplyCaps(address[],uint256[])",
         params: [[NEW_VTRX], ["10000000000000"]],
       },
+
+      {
+        target: COMPTROLLER,
+        signature: "_setCollateralFactor(address,uint256)",
+        params: [VSXP, parseUnits("0.175", 18)],
+      },
     ],
     meta,
     ProposalType.REGULAR,
@@ -71,6 +79,11 @@ forking(26881099, () => {
   });
 
   describe("Pre-VIP behavior", async () => {
+    it("collateral factor of SXP  equals 30%", async () => {
+      const newCollateralFactor = (await comptroller.markets(VSXP)).collateralFactorMantissa;
+      expect(newCollateralFactor).to.equal(parseUnits("0.25", 18));
+    });
+
     it("collateral factor of TRX (old) equals 30%", async () => {
       const newCollateralFactor = (await comptroller.markets(OLD_VTRX)).collateralFactorMantissa;
       expect(newCollateralFactor).to.equal(parseUnits("0.3", 18));
@@ -122,18 +135,23 @@ forking(26881099, () => {
     });
 
     it("supply cap of TRX (new) equals 10000000", async () => {
-      const oldCap = await comptroller.supplyCaps(NEW_VTRX);
-      expect(oldCap).to.equal(parseUnits("10000000", 6));
+      const newCap = await comptroller.supplyCaps(NEW_VTRX);
+      expect(newCap).to.equal(parseUnits("10000000", 6));
     });
 
     it("borrow cap of TRX (new) equals 8000000", async () => {
-      const oldCap = await comptroller.borrowCaps(NEW_VTRX);
-      expect(oldCap).to.equal(parseUnits("8000000", 6));
+      const newCap = await comptroller.borrowCaps(NEW_VTRX);
+      expect(newCap).to.equal(parseUnits("8000000", 6));
     });
 
     it("borrow cap of DAI equals 7500000", async () => {
-      const oldCap = await comptroller.borrowCaps(VDAI);
-      expect(oldCap).to.equal(parseUnits("7500000", 18));
+      const newCap = await comptroller.borrowCaps(VDAI);
+      expect(newCap).to.equal(parseUnits("7500000", 18));
+    });
+
+    it("collateral factor of SXP  equals 17.5%", async () => {
+      const newCollateralFactor = (await comptroller.markets(VSXP)).collateralFactorMantissa;
+      expect(newCollateralFactor).to.equal(parseUnits("0.175", 18));
     });
   });
 });
