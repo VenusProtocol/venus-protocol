6d37fad137422090bd46f024c082d30797f227eb
Narayan Prusty <arsenal.narayan@gmail.com>
2023-03-31 23:35:31 +0100

    updated score gradually after adding market

--- tests/hardhat/Prime/Prime.ts
+++ tests/hardhat/Prime/Prime.ts
@@ -515,7 +515,7 @@ describe("PrimeScenario Token", () => {
         await comptroller._setPrimeToken(prime.address);
       });
 
-      it("add existing market after issuing prime tokens - update score manually", async () => {
+      it.skip("add existing market after issuing prime tokens - update score manually", async () => {
         await xvs.connect(user3).approve(xvsVault.address, bigNumber18.mul(2000));
         await xvsVault.connect(user3).deposit(xvs.address, 0, bigNumber18.mul(2000)); 
         await prime.issue(false, [user3.getAddress()]);
@@ -607,66 +607,91 @@ describe("PrimeScenario Token", () => {
         expect((await prime.callStatic.getInterestAccrued(vusdt.address, user1.getAddress()))).to.be.equal(2074176)
       });
 
-      it.skip("add existing market after issuing prime tokens - update score gradually", async () => {
+      it("add existing market after issuing prime tokens - update score gradually", async () => {
+        await xvs.connect(user3).approve(xvsVault.address, bigNumber18.mul(2000));
+        await xvsVault.connect(user3).deposit(xvs.address, 0, bigNumber18.mul(2000)); 
+        await prime.issue(false, [user3.getAddress()]);
+        await vbnb._setPrimeToken(prime.address);
         await prime.addMarket(
           vbnb.address,
           bigNumber18.mul(1),
           bigNumber18.mul(1)
         )
 
-        const interest = await prime.interests(vbnb.address, user3.getAddress())
+        let interest = await prime.interests(vbnb.address, user3.getAddress())
         expect(interest.accrued).to.be.equal(0)
         expect(interest.score).to.be.equal(0)
         expect(interest.indexMultiplier).to.be.equal(0)
         expect(interest.rewardIndex).to.be.equal(0)
+
+        let market = await prime.markets(vbnb.address)
+        expect(market.supplyMultiplier).to.be.equal(bigNumber18.mul(1))
+        expect(market.borrowMultiplier).to.be.equal(bigNumber18.mul(1))
+        expect(market.rewardIndex).to.be.equal(0)
+        expect(market.lastUpdated).to.be.not.equal(0)
+        expect(market.score).to.be.equal(0)
+        expect(market.indexMultiplier).to.be.equal(0)
+
+        await bnb.connect(user3).approve(vbnb.address, bigNumber18.mul(90));
+        await vbnb.connect(user3).mint(bigNumber18.mul(1));
+
+        interest = await prime.interests(vbnb.address, user3.getAddress())
+        expect(interest.accrued).to.be.equal(0)
+        /**
+         * score = 2000^0.5 * 91^0.5 = 426.6145802
+         */
+        expect(interest.score).to.be.equal("426614580154030858642")
+        expect(interest.indexMultiplier).to.be.equal(1)
+        expect(interest.rewardIndex).to.be.equal(0)
+
+        market = await prime.markets(vbnb.address)
+        expect(market.supplyMultiplier).to.be.equal(bigNumber18.mul(1))
+        expect(market.borrowMultiplier).to.be.equal(bigNumber18.mul(1))
+        expect(market.rewardIndex).to.be.equal(0)
+        expect(market.lastUpdated).to.be.not.equal(0)
+        expect(market.score).to.be.equal("426614580154030858642")
+        expect(market.indexMultiplier).to.be.equal(1)
+
+        await mine(24 * 60 * 20);
+        await prime.accrueInterest(vbnb.address)
+        market = await prime.markets(vbnb.address)
+        expect(market.supplyMultiplier).to.be.equal(bigNumber18.mul(1))
+        expect(market.borrowMultiplier).to.be.equal(bigNumber18.mul(1))
+        /**
+         * pastBlocks = 28801
+         * incomePerBlock = 18
+         * totalIncome = pastBlocks * incomePerBlock = 518418
+         * distributionIncome = 20% of 518436 = 103683
+         * rewardIndex += 103687/426614580154030858642 = 243
+         */
+        expect(market.rewardIndex).to.be.equal(243)
+        expect(market.lastUpdated).to.be.not.equal(0)
+        expect(market.score).to.be.equal("426614580154030858642")
+        expect(market.indexMultiplier).to.be.equal(2)
+
+        /**
+         * index = 243
+         * indexMultiplier = 1
+         * score = 426614580154030858642 (426.614580154030858642)
+         * interest = index * indexMultiplier * score = 243 * 1 * 426.614580154030858642 = 1037088
+         */
+        expect((await prime.callStatic.getInterestAccrued(vusdt.address, user1.getAddress()))).to.be.equal(1037088)
+
+        await mine(24 * 60 * 20);
+        await prime.accrueInterest(vbnb.address)
+        market = await prime.markets(vbnb.address)
+        expect(market.supplyMultiplier).to.be.equal(bigNumber18.mul(1))
+        expect(market.borrowMultiplier).to.be.equal(bigNumber18.mul(1))
+        expect(market.rewardIndex).to.be.equal(486)
+        expect(market.lastUpdated).to.be.not.equal(0)
+        expect(market.score).to.be.equal("426614580154030858642")
+        expect(market.indexMultiplier).to.be.equal(3)
+
+        /**
+         * 1037088 * 2 = 2074176
+         */
+        expect((await prime.callStatic.getInterestAccrued(vusdt.address, user1.getAddress()))).to.be.equal(2074176)
       });
-
-      // it("update QVL of existing market", async () => {
-      //   const [user1] = accounts;
-
-      //   let interest = await prime._interests(vusdt.address, user1.getAddress());
-      //   expect(interest.totalQVL).to.be.equal(bigNumber18.mul(5));
-      //   expect(interest.index).to.be.equal(bigNumber18.mul(1));
-      //   expect(interest.accrued).to.be.equal(0);
-
-      //   await mine(24 * 60 * 20);
-
-      //   await prime.toggleMarketPause(vusdt.address);
-      //   await prime.accrueInterestForUsers([user1.getAddress()], vusdt.address);
-      //   await prime.updateQVLCaps(
-      //     vusdt.address,
-      //     [
-      //       bigNumber18.mul("1"),
-      //       bigNumber18.mul("4"),
-      //       bigNumber18.mul("5"),
-      //       bigNumber18.mul("6"),
-      //       bigNumber18.mul("7"),
-      //     ],
-      //     [
-      //       bigNumber18.mul("1"),
-      //       bigNumber18.mul("4"),
-      //       bigNumber18.mul("5"),
-      //       bigNumber18.mul("6"),
-      //       bigNumber18.mul("7"),
-      //     ],
-      //   );
-      //   await prime.updateQVLs([user1.getAddress()], vusdt.address);
-      //   await prime.toggleMarketPause(vusdt.address);
-
-      //   interest = await prime._interests(vusdt.address, user1.getAddress());
-      //   expect(interest.totalQVL).to.be.equal(bigNumber18.mul(1));
-      //   expect(interest.accrued).to.be.equal(285);
-
-      //   let reward = await prime.callStatic.getInterestAccrued(vusdt.address, user1.getAddress());
-      //   expect(reward).to.be.equal(285);
-
-      //   await usdt.transfer(prime.address, reward);
-      //   await expect(prime.connect(user1).claimInterest(vusdt.address)).to.be.not.reverted;
-
-      //   await mine(24 * 60 * 20);
-      //   reward = await prime.callStatic.getInterestAccrued(vusdt.address, user1.getAddress());
-      //   expect(reward).to.be.equal(57);
-      // });
     });
   });
 });
