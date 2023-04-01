11a9f112c0d88617c21df2743b7ef1bbad285e92
Jesus Lanchas <jesus.lanchas@daureos.com>
2023-04-01 20:46:29 +0200

    style: linter

--- script/hardhat/fork/vip-104.ts
+++ script/hardhat/fork/vip-104.ts
@@ -3,17 +3,16 @@ import { parseUnits } from "ethers/lib/utils";
 import { ethers } from "hardhat";
 
 import { Comptroller } from "../../../typechain";
+import COMPTROLLER_ABI from "./vip-104/comptroller.json";
 import { forking, pretendExecutingVip, testVip } from "./vip-framework";
 import { ProposalType } from "./vip-framework/types";
-import { makeProposal, setMaxStalePeriodInOracle, expectEvents } from "./vip-framework/utils";
+import { expectEvents, makeProposal, setMaxStalePeriodInOracle } from "./vip-framework/utils";
 
 const COMPTROLLER = "0xfd36e2c2a6789db23113685031d7f16329158384";
 const NEW_VTRX = "0xC5D3466aA484B040eE977073fcF337f2c00071c1";
 const OLD_VTRX = "0x61eDcFe8Dd6bA3c891CB9bEc2dc7657B3B422E93";
 const VDAI = "0x334b3eCB4DCa3593BCCC3c7EBD1A1C1d1780FBF1";
 const VSXP = "0x2fF3d0F6990a40261c66E1ff2017aCBc282EB6d0";
-
-import COMPTROLLER_ABI from "./vip-104/comptroller.json";
 
 export const vip104 = () => {
   const meta = {
@@ -119,7 +118,7 @@ forking(26881099, () => {
   });
 
   testVip("VIP-104 Risk Parameters Update", vip104(), {
-    callbackAfterExecution: async (txResponse) => {
+    callbackAfterExecution: async txResponse => {
       await expectEvents(
         txResponse,
         [COMPTROLLER_ABI],
--- script/hardhat/fork/vip-framework/utils.ts
+++ script/hardhat/fork/vip-framework/utils.ts
@@ -1,9 +1,9 @@
 import { defaultAbiCoder } from "@ethersproject/abi";
 import { impersonateAccount, setBalance } from "@nomicfoundation/hardhat-network-helpers";
 import { NumberLike } from "@nomicfoundation/hardhat-network-helpers/dist/src/types";
-import { ethers, network } from "hardhat";
 import { expect } from "chai";
-import { TransactionResponse, ContractInterface } from "ethers";
+import { ContractInterface, TransactionResponse } from "ethers";
+import { ethers, network } from "hardhat";
 
 import { Command, Proposal, ProposalMeta, ProposalType } from "./types";
 
@@ -63,32 +63,43 @@ export const makeProposal = (commands: Command[], meta: ProposalMeta, type: Prop
   };
 };
 
-export const setMaxStalePeriodInOracle = async (comptrollerAddress: string, maxStalePeriodInSeconds: number = 31536000 /* 1 year */) => {
+export const setMaxStalePeriodInOracle = async (
+  comptrollerAddress: string,
+  maxStalePeriodInSeconds: number = 31536000 /* 1 year */,
+) => {
   const comptroller = await ethers.getContractAt("Comptroller", comptrollerAddress);
   const oracle = await ethers.getContractAt("VenusChainlinkOracle", await comptroller.oracle());
   const oracleAdmin = await initMainnetUser(await oracle.admin(), ethers.utils.parseEther("1.0"));
 
-  const tx = await oracle
-    .connect(oracleAdmin)
-    .setMaxStalePeriod(maxStalePeriodInSeconds);
+  const tx = await oracle.connect(oracleAdmin).setMaxStalePeriod(maxStalePeriodInSeconds);
   await tx.wait();
 };
 
-export const expectEvents = async (txResponse: TransactionResponse, abis: ContractInterface[], expectedEvents: string[], expectedCounts: number[]) => {
+export const expectEvents = async (
+  txResponse: TransactionResponse,
+  abis: ContractInterface[],
+  expectedEvents: string[],
+  expectedCounts: number[],
+) => {
   const getNamedEvents = (abi: ContractInterface) => {
     const iface = new ethers.utils.Interface(abi);
-    return txResponse.events.map(it => {
-      try {
-        return iface.parseLog(it).name;
-      } catch(error) {
-        error; // shhh
-      }
-    }).filter(Boolean);
-  }
+    return txResponse.events
+      .map(it => {
+        try {
+          return iface.parseLog(it).name;
+        } catch (error) {
+          error; // shhh
+        }
+      })
+      .filter(Boolean);
+  };
 
   const namedEvents = abis.flatMap(getNamedEvents);
 
   for (let i = 0; i < expectedEvents.length; ++i) {
-    expect(namedEvents.filter(it => it === expectedEvents[i]), `expected a differnt number of ${expectedEvents[i]} events`).to.have.lengthOf(expectedCounts[i]);
+    expect(
+      namedEvents.filter(it => it === expectedEvents[i]),
+      `expected a differnt number of ${expectedEvents[i]} events`,
+    ).to.have.lengthOf(expectedCounts[i]);
   }
 };
