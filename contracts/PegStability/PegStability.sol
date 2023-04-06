06073f3c4cb773d0a38ca30623d51dab6230cd44
0xLucian <0xluciandev@gmail.com>
2023-04-06 17:39:07 +0300

    first draft implementation of main functionality of PSM

--- contracts/PegStability/PegStability.sol
+++ contracts/PegStability/PegStability.sol
@@ -3,14 +3,37 @@ pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 import "@venusprotocol/governance-contracts/contracts/Governance/AccessControlledV8.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
+
+interface VAI {
+    function balanceOf(address usr) external returns (uint256);
+
+    function transferFrom(address src, address dst, uint amount) external returns (bool);
+
+    function mint(address usr, uint wad) external;
+
+    function burn(address usr, uint wad) external;
+}
+
+// NOTE: WE ASSUME that VAI and stableToken both have 18 decimal places which is the case for BSC
 
 contract PegStability is AccessControlledV8, ReentrancyGuardUpgradeable {
+    using SafeERC20Upgradeable for IERC20Upgradeable;
+
+    // Helper enum for calculation of the fee
+    enum FeeDirection {
+        IN,
+        OUT
+    }
     address public immutable vaiAddress;
-    address public immutable stableToken;
-    uint256 public constant BASIS_POINTS_DIVISOR = 1000;
-    uint256 public feeIn; // fee is in basis points
-    uint256 public feeOut; // fee is in basis points
+    address public immutable stableTokenAddress;
+    address public venusTreasury;
+    uint256 public constant BASIS_POINTS_DIVISOR = 10000; // fee is in basis points
+    uint256 public feeIn; // incoming VAI fee
+    uint256 public feeOut; // outgoing VAI fee
     uint256 public vaiMintCap; // max amount of VAI that can be minted through this contract
+    uint256 public vaiMinted;
     bool public isPaused;
 
     /**
@@ -26,14 +49,17 @@ contract PegStability is AccessControlledV8, ReentrancyGuardUpgradeable {
     /// @notice Event emitted when the contract is resumed after pause
     event PsmResumed(address indexed admin);
 
-    /// @notice Event emitted when the feeIn state var is modified
+    /// @notice Event emitted when feeIn state var is modified
     event FeeInChanged(uint256 oldFeeIn, uint256 newFeeIn);
 
-    /// @notice Event emitted when the feeOut state var is modified
+    /// @notice Event emitted when feeOut state var is modified
     event FeeOutChanged(uint256 oldFeeOut, uint256 newFeeOut);
 
-    /// @notice Event emitted when the vaiMintCap state var is modified
+    /// @notice Event emitted when vaiMintCap state var is modified
     event VaiMintCapChanged(uint256 oldCap, uint256 newCap);
+
+    /// @notice Event emitted when venusTreasury state var is modified
+    event VenusTreasuryChanged(address oldTreasury, address newTreasury);
 
     /**
      * @dev Prevents functions to execute when contract is paused.
@@ -44,30 +70,82 @@ contract PegStability is AccessControlledV8, ReentrancyGuardUpgradeable {
     }
 
     /// @custom:oz-upgrades-unsafe-allow constructor
-    constructor(address stableToken_, address vaiAddress_) {
-        ensureNonzeroAddress(stableToken_);
+    constructor(address stableTokenAddress_, address vaiAddress_) {
+        ensureNonzeroAddress(stableTokenAddress_);
         ensureNonzeroAddress(vaiAddress_);
-        stableToken = stableToken_;
+        stableTokenAddress = stableTokenAddress_;
         vaiAddress = vaiAddress_;
         _disableInitializers();
     }
 
     function initialize(
         address accessControlManager_,
+        address venusTreasury_,
         uint256 feeIn_,
         uint256 feeOut_,
         uint256 vaiMintCap_
     ) external initializer {
+        ensureNonzeroAddress(accessControlManager_);
+        ensureNonzeroAddress(venusTreasury_);
         __AccessControlled_init(accessControlManager_);
         feeIn = feeIn_;
         feeOut = feeOut_;
         vaiMintCap = vaiMintCap_;
+        venusTreasury = venusTreasury_;
     }
 
     /// @notice Checks the passed address is nonzero
     function ensureNonzeroAddress(address someone) private pure {
         require(someone != address(0), "can't be zero address");
     }
+
+    /*** Swap Functions ***/
+
+    function swapVAIForStable(address receiver, uint256 amount) external isActive {
+        ensureNonzeroAddress(receiver);
+        require(VAI(vaiAddress).balanceOf(msg.sender) >= amount, "not enought VAI");
+        uint256 fee = _calculateFee(amount, FeeDirection.IN);
+        bool success = VAI(vaiAddress).transferFrom(msg.sender, venusTreasury, fee);
+        require(success, "VAI fee transfer failed");
+        //also the same amount will be transfered in stable to receiver
+        uint256 amountToBurn = amount - fee;
+        VAI(vaiAddress).burn(msg.sender, amountToBurn);
+        IERC20Upgradeable(stableTokenAddress).safeTransferFrom(address(this), receiver, amountToBurn);
+    }
+
+    function swapStableForVAI(address receiver, uint256 amount) external isActive {
+        ensureNonzeroAddress(receiver);
+        require(amount > 0, "Amount must be greater than zero");
+        IERC20Upgradeable(stableTokenAddress).safeTransferFrom(msg.sender, address(this), amount);
+        require(vaiMinted + amount <= vaiMintCap, "VAI min cap reached");
+        //calculate feeIn
+        uint256 fee = _calculateFee(amount, FeeDirection.OUT);
+        uint256 vaiToSend = amount - fee;
+        // mint VAI to receiver
+        VAI(vaiAddress).mint(receiver, vaiToSend);
+        // mint VAI fee to venus treasury
+        VAI(vaiAddress).mint(venusTreasury, vaiToSend);
+    }
+
+    /*** Helper Functions ***/
+
+    function _calculateFee(uint256 amount, FeeDirection direction) internal view returns (uint256) {
+        uint256 feePercent;
+        if (direction == FeeDirection.IN) {
+            feePercent = feeIn;
+        } else {
+            feePercent = feeOut;
+        }
+        if (feePercent == 0) {
+            return amount;
+        } else {
+            // checking if the percent calculation will result in rounding down to 0
+            require(amount * feePercent >= BASIS_POINTS_DIVISOR, "amount too small");
+            return (amount * feePercent) / BASIS_POINTS_DIVISOR;
+        }
+    }
+
+    /*** Admin Functions ***/
 
     function pause() external {
         _checkAccessAllowed("pause()");
@@ -107,5 +185,13 @@ contract PegStability is AccessControlledV8, ReentrancyGuardUpgradeable {
         uint256 oldVaiMintCap = vaiMintCap;
         vaiMintCap = vaiMintCap_;
         emit VaiMintCapChanged(oldVaiMintCap, vaiMintCap);
+    }
+
+    function setVenusTreasury(address venusTreasury_) external {
+        _checkAccessAllowed("setVenusTreasury(address)");
+        ensureNonzeroAddress(venusTreasury_);
+        address oldTreasuryAddress = venusTreasury;
+        venusTreasury = venusTreasury_;
+        emit VenusTreasuryChanged(oldTreasuryAddress, venusTreasury_);
     }
 }
