69b404a63e5ed5f09cd04514935721cd89f0f396
0xLucian <0xluciandev@gmail.com>
2023-05-09 09:47:19 +0300

    feat: add fork syntactic sugar and replace QUICKNODE Key with whole URI in env

--- tests/hardhat/Fork/utils.ts
+++ tests/hardhat/Fork/utils.ts
@@ -1,4 +1,20 @@
+import { impersonateAccount } from "@nomicfoundation/hardhat-network-helpers";
+import { ethers } from "hardhat";
 import { network } from "hardhat";
+
+async function setForkBlock(blockNumber: number) {
+  await network.provider.request({
+    method: "hardhat_reset",
+    params: [
+      {
+        forking: {
+          jsonRpcUrl: process.env.QUICK_NODE_URL,
+          blockNumber,
+        },
+      },
+    ],
+  });
+}
 
 export const forking = (blockNumber: number, fn: () => void) => {
   describe(`At block #${blockNumber}`, () => {
@@ -9,16 +25,9 @@ export const forking = (blockNumber: number, fn: () => void) => {
   });
 };
 
-export async function setForkBlock(blockNumber: number) {
-  await network.provider.request({
-    method: "hardhat_reset",
-    params: [
-      {
-        forking: {
-          jsonRpcUrl: process.env.BSC_ARCHIVE_NODE,
-          blockNumber: blockNumber,
-        },
-      },
-    ],
-  });
-}
+export const initMainnetUser = async (user: string) => {
+  await impersonateAccount(user);
+  return ethers.getSigner(user);
+};
+
+export const FORK_MAINNET = process.env.FORK_MAINNET === "true";
