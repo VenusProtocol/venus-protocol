f91f43e66da911f6ffe8f315bfe93d7455c71bc4
Narayan Prusty <arsenal.narayan@gmail.com>
2023-07-28 11:19:05 +0100

    chore: use user instead of owner/account

--- contracts/Tokens/Prime/IPrime.sol
+++ contracts/Tokens/Prime/IPrime.sol
@@ -1,11 +1,11 @@
 pragma solidity ^0.5.16;
 
 interface IPrime {
-    function xvsUpdated(address owner) external;
+    function xvsUpdated(address user) external;
 
-    function executeBoost(address account, address vToken) external;
+    function executeBoost(address user, address vToken) external;
 
     function accrueInterest(address vToken) external;
 
-    function updateScore(address account, address vToken) external;
+    function updateScore(address user, address vToken) external;
 }
