86e70374a0d96a831ecd38ef91913c6157e1fdfa
Debugger022 <debugger022@gmail.com>
2023-10-04 08:57:51 +0530

    fix: ven-2016 n-36

--- contracts/Tokens/Prime/PrimeStorage.sol
+++ contracts/Tokens/Prime/PrimeStorage.sol
@@ -43,7 +43,7 @@ contract PrimeStorageV1 {
     uint256 public constant MAXIMUM_XVS_CAP = 1e5 * EXP_SCALE;
 
     /// @notice number of days user need to stake to claim prime token
-    uint256 public constant STAKING_PERIOD = 90 * 24 * 60 * 60;
+    uint256 public constant STAKING_PERIOD = 90 days;
 
     /// @notice maximum BPS = 100%
     uint256 internal constant MAXIMUM_BPS = 1e4;
