3cb76189c4ed1ff39afa04da4b7e40a58025f67d
Debugger022 <debugger022@gmail.com>
2023-10-04 07:58:39 +0530

    fix: ven-2016 n-23

--- contracts/Tokens/Prime/PrimeStorage.sol
+++ contracts/Tokens/Prime/PrimeStorage.sol
@@ -32,16 +32,16 @@ contract PrimeStorageV1 {
     uint256 internal constant EXP_SCALE = 1e18;
 
     /// @notice minimum amount of XVS user needs to stake to become a prime member
-    uint256 public constant MINIMUM_STAKED_XVS = 1000 * EXP_SCALE;
+    uint256 public constant MINIMUM_STAKED_XVS = 1e3 * EXP_SCALE;
 
     /// @notice maximum XVS taken in account when calculating user score
-    uint256 public constant MAXIMUM_XVS_CAP = 100000 * EXP_SCALE;
+    uint256 public constant MAXIMUM_XVS_CAP = 1e5 * EXP_SCALE;
 
     /// @notice number of days user need to stake to claim prime token
     uint256 public constant STAKING_PERIOD = 90 * 24 * 60 * 60;
 
     /// @notice maximum BPS = 100%
-    uint256 internal constant MAXIMUM_BPS = 10000;
+    uint256 internal constant MAXIMUM_BPS = 1e4;
 
     /// @notice Mapping to get prime token's metadata
     mapping(address => Token) public tokens;
