b9e3938dd2cba7605ff3aeb927dfb8ae6840a59e
GitGuru7 <128375421+GitGuru7@users.noreply.github.com>
2023-10-30 17:06:59 +0530

    fix: VMC-9

--- contracts/Governance/VTreasuryV8.sol
+++ contracts/Governance/VTreasuryV8.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.8.20;
+pragma solidity 0.8.19;
 
 import { SafeERC20, IERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -87,7 +87,7 @@ const config: HardhatUserConfig = {
         },
       },
       {
-        version: "0.8.20",
+        version: "0.8.19",
         settings: {
           optimizer: {
             enabled: true,
