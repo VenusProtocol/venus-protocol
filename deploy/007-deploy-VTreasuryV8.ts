e470b534e77168b0e2124c075f2a922bbd72b43b
GitGuru7 <128375421+GitGuru7@users.noreply.github.com>
2023-11-07 15:32:55 +0530

    feat: add sepolia deployments of treasury

--- deploy/007-deploy-VTreasuryV8.ts
+++ deploy/007-deploy-VTreasuryV8.ts
@@ -7,7 +7,7 @@ interface AdminAccounts {
 }
 const acmAdminAccount: AdminAccounts = {
   sepolia: "0x94fa6078b6b8a26f0b6edffbe6501b22a10470fb", // SEPOLIA MULTISIG
-  ethereum: "", // TODO: add Ethereum MULTISIG once it is deployed
+  ethereum: "0x285960C5B22fD66A736C7136967A3eB15e93CC67", // ETHEREUM MULTISIG
 };
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
