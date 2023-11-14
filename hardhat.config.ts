3242fdc80656229b333d94faa787aa36705114ae
Corey Rice <coreyarice@gmail.com>
2023-11-14 13:14:09 -0300

    refactor: allow for mainnet url to be set via env var

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -101,7 +101,7 @@ const config: HardhatUserConfig = {
       timeout: 12000000,
     },
     bscmainnet: {
-      url: "http://127.0.0.1:1248",
+      url: process.env.BSC_ARCHIVE_NODE_URL || "https://bsc-dataseed.binance.org/",
       chainId: 56,
       live: true,
       timeout: 1200000, // 20 minutes
