f06d9bcf4ffddfa30d21a795381aee864b48ba74
narayanprusty <arsenal.narayan@gmail.com>
2023-12-28 12:59:58 +0530

    fix: deployed xvs vault

--- deploy/009-deploy-xvs-vault.ts
+++ deploy/009-deploy-xvs-vault.ts
@@ -1,31 +1,16 @@
 import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 
-interface NetworkConfig {
-  useBlocks: boolean;
-  blocksPerYear?: number;
-}
-
-const networkConfig: { [key: string]: NetworkConfig } = {
-  bsctestnet: {
-    useBlocks: true,
-    blocksPerYear: 10512000,
-  },
-};
-
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts } = hre;
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
 
-  const networkArgs = networkConfig[hre.network.name];
-  const xvsVaultArgs = networkArgs.useBlocks ? [false, networkArgs.blocksPerYear] : [true, 0];
-
   await deployments.delete("XVSVault");
 
   await deploy("XVSVault", {
     from: deployer,
-    args: xvsVaultArgs,
+    args: [],
     log: true,
     autoMine: true,
   });
