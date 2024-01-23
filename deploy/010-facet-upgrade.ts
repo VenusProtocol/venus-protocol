9089389ae6a7969383d01570d31a97844fdcc730
narayanprusty <arsenal.narayan@gmail.com>
2024-01-23 16:05:41 +0530

    fix: deployed setter fac

---
+++ deploy/010-facet-upgrade.ts
@@ -0,0 +1,20 @@
+import { DeployFunction } from "hardhat-deploy/types";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts } = hre;
+  const { deploy } = deployments;
+
+  const { deployer } = await getNamedAccounts();
+
+  await deploy("SetterFacet", {
+    from: deployer,
+    args: [],
+    log: true,
+    autoMine: true,
+  });
+};
+
+func.tags = ["FacetUpgrade"];
+
+export default func;
