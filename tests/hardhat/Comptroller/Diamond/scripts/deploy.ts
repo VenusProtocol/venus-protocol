eabcb43f10b53d1509ca38db493dd98ad2dec522
narayanprusty <arsenal.narayan@gmail.com>
2024-02-02 14:44:45 +0530

    fix: removed log

--- tests/hardhat/Comptroller/Diamond/scripts/deploy.ts
+++ tests/hardhat/Comptroller/Diamond/scripts/deploy.ts
@@ -27,10 +27,6 @@ export async function deployFacets() {
 
     const FacetInterface = await ethers.getContractAt(`I${FacetName}`, facet.address);
 
-    if (FacetName === "PolicyFacet") {
-      console.log(getSelectors(FacetInterface));
-    }
-
     cut.push({
       facetAddress: facet.address,
       action: FacetCutAction.Add,
