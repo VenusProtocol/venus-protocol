a2a22b2993c1ac5deb9d8bade5e3ab2c2744cfed
Narayan Prusty <arsenal.narayan@gmail.com>
2024-02-19 16:22:39 +0530

    fix: use funcs from utils

--- tests/hardhat/Fork/XVSVaultUpgradeTimeManager.ts
+++ tests/hardhat/Fork/XVSVaultUpgradeTimeManager.ts
@@ -2,38 +2,16 @@ import { smock } from "@defi-wonderland/smock";
 import { impersonateAccount } from "@nomicfoundation/hardhat-network-helpers";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import chai from "chai";
-import { ethers, network } from "hardhat";
+import { ethers } from "hardhat";
 
 import { XVSVault, XVSVaultProxy } from "../../../typechain";
+import { forking } from "./utils";
 
 const { expect } = chai;
 chai.use(smock.matchers);
 
 const XVS_VAULT_PROXY = "0x9aB56bAD2D7631B2A857ccf36d998232A8b82280";
 const TIMELOCK = "0xce10739590001705F7FF231611ba4A48B2820327";
-
-export async function setForkBlock(blockNumber: number) {
-  await network.provider.request({
-    method: "hardhat_reset",
-    params: [
-      {
-        forking: {
-          jsonRpcUrl: process.env[`ARCHIVE_NODE_${process.env.FORKED_NETWORK}`],
-          blockNumber: blockNumber,
-        },
-      },
-    ],
-  });
-}
-
-const forking = (blockNumber: number, fn: () => void) => {
-  describe(`Reward and Setter facet upgrade check At block #${blockNumber}`, () => {
-    before(async () => {
-      await setForkBlock(blockNumber);
-    });
-    fn();
-  });
-};
 
 forking(36322844, () => {
   let xvsVault: XVSVault;
