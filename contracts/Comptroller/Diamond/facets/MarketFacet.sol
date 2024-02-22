e81ab4feaa5a2e62c27a5532251931b3ce749741
Debugger022 <debugger022@gmail.com>
2024-02-22 13:45:30 +0530

    fix: VPB-01

--- contracts/Comptroller/Diamond/facets/MarketFacet.sol
+++ contracts/Comptroller/Diamond/facets/MarketFacet.sol
@@ -203,10 +203,10 @@ contract MarketFacet is IMarketFacet, FacetBase {
      *  If allowed, the delegate will be able to borrow funds on behalf of the sender
      *  Upon a delegated borrow, the delegate will receive the funds, and the borrower
      *  will see the debt on their account
-     *  Upon a delegated redeem, the delegate will redeem the vTokens or the underlying,
-     *  will receive the redeemed amount on behalf of the approver(redeemer)
+     *  Upon a delegated redeem, the delegate will receive the redeemed amount and the approver
+     *  will see a deduction in his vToken balance
      * @param delegate The address to update the rights for
-     * @param approved Whether to grant (true) or revoke (false) the rights
+     * @param approved Whether to grant (true) or revoke (false) the borrowing or redeeming rights
      */
     function updateDelegate(address delegate, bool approved) external {
         _updateDelegate(msg.sender, delegate, approved);
