d3443c1ca51f6635da0b66ac75b8410c2c5e0c6b
Kirill Kuvshinov <kkirka@gmail.com>
2024-03-06 14:26:19 +0300

    feat: add sweepToken and sweepNative

--- contracts/Liquidator/LiquidateAndRedeem.sol
+++ contracts/Liquidator/LiquidateAndRedeem.sol
@@ -2,20 +2,27 @@
 
 pragma solidity 0.8.13;
 
+import { Ownable2Step } from "@openzeppelin/contracts/access/Ownable2Step.sol";
 import { IERC20Upgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+import { SafeERC20Upgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
 import { IVBNB, IVBep20 } from "../InterfacesV8.sol";
 import { approveOrRevert } from "../lib/approveOrRevert.sol";
+import { ensureNonzeroAddress } from "@venusprotocol/solidity-utilities/contracts/validators.sol";
 
 uint256 constant NO_ERROR = 0;
 
-contract LiquidateAndRedeem {
+contract LiquidateAndRedeem is Ownable2Step {
+    using SafeERC20Upgradeable for IERC20Upgradeable;
+
     IVBNB public immutable V_NATIVE;
 
     error LiquidationFailed(uint256 errCode);
     error RedeemFailed(uint256 errCode);
     error NativeTransferFailed();
 
-    constructor(IVBNB vNative) {
+    constructor(address owner_, IVBNB vNative) {
+        ensureNonzeroAddress(owner_);
+        _transferOwnership(owner_);
         V_NATIVE = vNative;
     }
 
@@ -25,6 +32,14 @@ contract LiquidateAndRedeem {
         _liquidateSeizingVNative(borrower, vTokenBorrowed);
         _redeemAllNative();
         _transferAllNative(receiver);
+    }
+
+    function sweepNative(address receiver) external onlyOwner {
+        _transferAllNative(receiver);
+    }
+
+    function sweepTokens(IERC20Upgradeable token, address receiver) external onlyOwner {
+        _transferAll(token, receiver);
     }
 
     function _liquidateSeizingVNative(address borrower, IVBep20 vTokenBorrowed) internal {
@@ -51,5 +66,9 @@ contract LiquidateAndRedeem {
         if (!success) {
             revert NativeTransferFailed();
         }
+    }
+
+    function _transferAll(IERC20Upgradeable token, address receiver) internal {
+        token.safeTransfer(receiver, token.balanceOf(address(this)));
     }
 }
