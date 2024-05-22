ba5b781666d6963f118eb972b0ac214a4bbf64c3
GitGuru7 <128375421+GitGuru7@users.noreply.github.com>
2024-05-22 17:13:04 +0530

    feat: add VTreasuryV8 deployments for X Layer

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -183,6 +183,18 @@ const config: HardhatUserConfig = {
       live: true,
       accounts: DEPLOYER_PRIVATE_KEY ? [`0x${DEPLOYER_PRIVATE_KEY}`] : [],
     },
+    xlayertestnet: {
+      url: process.env.ARCHIVE_NODE_xlayertestnet || "https://testrpc.xlayer.tech/",
+      chainId: 195,
+      live: true,
+      accounts: DEPLOYER_PRIVATE_KEY ? [`0x${DEPLOYER_PRIVATE_KEY}`] : [],
+    },
+    xlayermainnet: {
+      url: process.env.ARCHIVE_NODE_xlayermainnet || "https://rpc.xlayer.tech/",
+      chainId: 196,
+      live: true,
+      accounts: DEPLOYER_PRIVATE_KEY ? [`0x${DEPLOYER_PRIVATE_KEY}`] : [],
+    },
   },
   etherscan: {
     apiKey: {
@@ -194,6 +206,8 @@ const config: HardhatUserConfig = {
       opbnbmainnet: process.env.ETHERSCAN_API_KEY || "ETHERSCAN_API_KEY",
       arbitrumsepolia: process.env.ETHERSCAN_API_KEY || "ETHERSCAN_API_KEY",
       arbitrumone: process.env.ETHERSCAN_API_KEY || "ETHERSCAN_API_KEY",
+      xlayertestnet: process.env.ETHERSCAN_API_KEY || "ETHERSCAN_API_KEY",
+      xlayermainnet: process.env.ETHERSCAN_API_KEY || "ETHERSCAN_API_KEY",
     },
     customChains: [
       {
@@ -258,6 +272,22 @@ const config: HardhatUserConfig = {
         urls: {
           apiURL: `https://api.arbiscan.io/api/`,
           browserURL: "https://arbiscan.io/",
+        },
+      },
+      {
+        network: "xlayertestnet",
+        chainId: 195,
+        urls: {
+          apiURL: "https://www.oklink.com/api/v5/explorer/contract/verify-source-code-plugin/XLAYER_TESTNET",
+          browserURL: "https://www.oklink.com/xlayer-test",
+        },
+      },
+      {
+        network: "xlayermainnet",
+        chainId: 196,
+        urls: {
+          apiURL: "https://www.oklink.com/api/v5/explorer/contract/verify-source-code-plugin/XLAYER",
+          browserURL: "https://www.oklink.com/xlayer",
         },
       },
     ],
