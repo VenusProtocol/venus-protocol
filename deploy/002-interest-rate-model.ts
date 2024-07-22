21f12bd16a5a84afc2de562f82ff53a59f55e443
Narayan Prusty <arsenal.narayan@gmail.com>
2024-07-22 12:46:12 +0530

    fix: fixed deploy script

--- deploy/002-interest-rate-model.ts
+++ deploy/002-interest-rate-model.ts
@@ -2,38 +2,41 @@ import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const { deployments, getNamedAccounts } = hre;
+  const { deployments, getNamedAccounts, network } = hre;
   const { deploy } = deployments;
 
   const { deployer } = await getNamedAccounts();
 
-  await deploy("InterestRateModelVUSDC", {
-    contract: "JumpRateModel",
-    from: deployer,
-    log: true,
-    autoMine: true,
-    args: [0, "50000000000000000", "1090000000000000000", "800000000000000000"],
-  });
+  if (!network.live) {
+    await deploy("InterestRateModelVUSDC", {
+      contract: "JumpRateModel",
+      from: deployer,
+      log: true,
+      autoMine: true,
+      args: [0, "50000000000000000", "1090000000000000000", "800000000000000000"],
+    });
 
-  await deploy("InterestRateModelVETH", {
-    contract: "JumpRateModel",
-    from: deployer,
-    log: true,
-    autoMine: true,
-    args: [0, "40000000000000000", "1080000000000000000", "700000000000000000"],
-  });
+    await deploy("InterestRateModelVETH", {
+      contract: "JumpRateModel",
+      from: deployer,
+      log: true,
+      autoMine: true,
+      args: [0, "40000000000000000", "1080000000000000000", "700000000000000000"],
+    });
+  }
 
-  await deploy("InterestRateModelVBNB", {
-    contract: "JumpRateModel",
-    from: deployer,
-    log: true,
-    autoMine: true,
-    args: [0, "225000000000000000", "6800000000000000000", "700000000000000000"],
-  });
+  if (network.name === "bscmainnet") {
+    await deploy("InterestRateModelVBNB", {
+      contract: "JumpRateModel",
+      from: deployer,
+      log: true,
+      autoMine: true,
+      args: [0, "225000000000000000", "6800000000000000000", "700000000000000000"],
+    });
+  }
 };
 
 func.tags = ["InterestRateModel"];
-// These contracts are not deployed on a live network so we only want to try and deploy them locally
-func.skip = async hre => hre.network.name !== "hardhat";
+func.skip = async hre => hre.network.name !== "hardhat" && hre.network.name !== "bscmainnet";
 
 export default func;
