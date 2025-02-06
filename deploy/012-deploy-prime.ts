3b8f9cec3e1ffbd6294435742a4fd63925ce547e
GitGuru7 <128375421+GitGuru7@users.noreply.github.com>
2025-02-06 14:15:36 +0530

    feat: deploy & configure prime on unichain sepolia

--- deploy/012-deploy-prime.ts
+++ deploy/012-deploy-prime.ts
@@ -27,6 +27,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     arbitrumsepolia: TEN_MINUTES,
     zksyncsepolia: TEN_MINUTES,
     opsepolia: TEN_MINUTES,
+    unichainsepolia: TEN_MINUTES,
     bscmainnet: NINETY_DAYS,
     ethereum: NINETY_DAYS,
     arbitrumone: NINETY_DAYS,
@@ -45,6 +46,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     zksyncmainnet: 0,
     opsepolia: 0,
     opmainnet: 0,
+    unichainsepolia: 0,
     hardhat: 0,
   };
 
@@ -57,6 +59,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     zksyncmainnet: 0, // time based contracts
     opsepolia: 0, // time based contracts
     opmainnet: 0, // time based contracts
+    unichainsepolia: 0, // time based contracts
     bscmainnet: 10_512_000,
     ethereum: 2_628_000,
     hardhat: 100,
@@ -89,6 +92,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     zksyncmainnet: "0x751Aa759cfBB6CE71A43b48e40e1cCcFC66Ba4aa", // ZKSYNC MAINNET MULTISIG
     opsepolia: "0xd57365EE4E850e881229e2F8Aa405822f289e78d", // OPSEPOLIA MULTISIG
     opmainnet: "0x2e94dd14E81999CdBF5deDE31938beD7308354b3", // OPMAINNET MULTISIG
+    unichainsepolia: "0x9831D3A641E8c7F082EEA75b8249c99be9D09a34", // UNICHAIN SEPOLIA MULTISIG
     bscmainnet: await getContractAddressOrNullAddress(deployments, "NormalTimelock"),
     bsctestnet: await getContractAddressOrNullAddress(deployments, "NormalTimelock"),
   };
