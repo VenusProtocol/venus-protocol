f4c29d1b62e7672265a6b63e9752d0bfe1e9a370
Kirill Kuvshinov <kkirka@gmail.com>
2025-04-07 10:56:39 +0300

    feat: add CheckpointRateModel

---
+++ contracts/InterestRateModels/CheckpointRateModel.sol
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: BSD-3-Clause
+pragma solidity 0.8.25;
+
+import { ensureNonzeroAddress } from "@venusprotocol/solidity-utilities/contracts/validators.sol";
+import { InterestRateModelV8 } from "./InterestRateModelV8.sol";
+
+/**
+ * @title Venus CheckpointRateModel Contract
+ * @notice A contract that applies a rate model based on whether a certain checkpoint
+ *   in time has passed. Useful to adjust interest rates for block time changes in
+ *   the underlying networks, if the block time update is scheduled in advance.
+ * @author Venus
+ */
+contract CheckpointRateModel is InterestRateModelV8 {
+    InterestRateModelV8 public immutable OLD_RATE_MODEL;
+    InterestRateModelV8 public immutable NEW_RATE_MODEL;
+    uint256 public immutable CHECKPOINT_TIMESTAMP;
+
+    /**
+     * @notice Constructor
+     * @param oldRateModel Rate model to use before the checkpoint
+     * @param newRateModel Rate model to use after the checkpoint
+     * @param checkpointTimestamp Checkpoint timestamp
+     */
+    constructor(InterestRateModelV8 oldRateModel, InterestRateModelV8 newRateModel, uint256 checkpointTimestamp) {
+        ensureNonzeroAddress(address(oldRateModel));
+        ensureNonzeroAddress(address(newRateModel));
+        OLD_RATE_MODEL = oldRateModel;
+        NEW_RATE_MODEL = newRateModel;
+        CHECKPOINT_TIMESTAMP = checkpointTimestamp;
+    }
+
+    /**
+     * @notice Returns the current rate model contract (either the old one or the new one)
+     * @return Rate model in use
+     */
+    function currentRateModel() external view returns (InterestRateModelV8) {
+        return _getCurrentRateModel();
+    }
+
+    /**
+     * @inheritdoc InterestRateModelV8
+     */
+    function getBorrowRate(uint256 cash, uint256 borrows, uint256 reserves) external view override returns (uint256) {
+        return _getCurrentRateModel().getBorrowRate(cash, borrows, reserves);
+    }
+
+    /**
+     * @inheritdoc InterestRateModelV8
+     */
+    function getSupplyRate(
+        uint256 cash,
+        uint256 borrows,
+        uint256 reserves,
+        uint256 reserveFactorMantissa
+    ) external view override returns (uint256) {
+        return _getCurrentRateModel().getSupplyRate(cash, borrows, reserves, reserveFactorMantissa);
+    }
+
+    /**
+     * @dev Returns the current rate model contract (either the old one or the new one)
+     * @return Rate model in use
+     */
+    function _getCurrentRateModel() internal view returns (InterestRateModelV8) {
+        return (block.timestamp >= CHECKPOINT_TIMESTAMP) ? NEW_RATE_MODEL : OLD_RATE_MODEL;
+    }
+}
---
+++ tests/hardhat/InterestRateModels/CheckpointRateModel.ts
@@ -0,0 +1,82 @@
+import { FakeContract, smock } from "@defi-wonderland/smock";
+import { loadFixture, time } from "@nomicfoundation/hardhat-network-helpers";
+import chai from "chai";
+import { parseUnits } from "ethers/lib/utils";
+import { ethers } from "hardhat";
+
+import { CheckpointRateModel, InterestRateModelV8 } from "../../../typechain";
+
+const { expect } = chai;
+chai.use(smock.matchers);
+
+describe("CheckpointRateModel tests", () => {
+  let checkpointRateModel: CheckpointRateModel;
+  let oldRateModel: FakeContract<InterestRateModelV8>;
+  let newRateModel: FakeContract<InterestRateModelV8>;
+  let checkpointTimestamp: number;
+  const cash = parseUnits("10", 18);
+  const borrows = parseUnits("4", 18);
+  const reserves = parseUnits("2", 18);
+  const reserveFactorMantissa = parseUnits("0.1", 18);
+
+  const fixture = async () => {
+    const interestRateModelFactory = await ethers.getContractFactory("WhitePaperInterestRateModel");
+    oldRateModel = await smock.fake<InterestRateModelV8>(interestRateModelFactory.interface);
+    newRateModel = await smock.fake<InterestRateModelV8>(interestRateModelFactory.interface);
+
+    const checkpointRateModelFactory = await ethers.getContractFactory("CheckpointRateModel");
+    checkpointTimestamp = (await time.latest()) + 100; // 100 seconds in the future
+    checkpointRateModel = await checkpointRateModelFactory.deploy(
+      oldRateModel.address,
+      newRateModel.address,
+      checkpointTimestamp,
+    );
+    await checkpointRateModel.deployed();
+  };
+
+  beforeEach(async () => {
+    await loadFixture(fixture);
+  });
+
+  it("should revert if oldRateModel address is zero", async () => {
+    const checkpointRateModelFactory = await ethers.getContractFactory("CheckpointRateModel");
+    await expect(
+      checkpointRateModelFactory.deploy(ethers.constants.AddressZero, newRateModel.address, checkpointTimestamp),
+    ).to.be.revertedWithCustomError(checkpointRateModelFactory, "ZeroAddressNotAllowed");
+  });
+
+  it("should revert if newRateModel address is zero", async () => {
+    const checkpointRateModelFactory = await ethers.getContractFactory("CheckpointRateModel");
+    await expect(
+      checkpointRateModelFactory.deploy(oldRateModel.address, ethers.constants.AddressZero, checkpointTimestamp),
+    ).to.be.revertedWithCustomError(checkpointRateModelFactory, "ZeroAddressNotAllowed");
+  });
+
+  it("should use old rate model before checkpoint", async () => {
+    await time.increaseTo(checkpointTimestamp - 1);
+
+    oldRateModel.getBorrowRate.returns(100);
+    oldRateModel.getSupplyRate.returns(50);
+
+    expect(await checkpointRateModel.getBorrowRate(cash, borrows, reserves)).to.equal(100);
+    expect(await checkpointRateModel.getSupplyRate(cash, borrows, reserves, reserveFactorMantissa)).to.equal(50);
+  });
+
+  it("should use new rate model after checkpoint", async () => {
+    await time.increaseTo(checkpointTimestamp);
+
+    newRateModel.getBorrowRate.returns(200);
+    newRateModel.getSupplyRate.returns(100);
+
+    expect(await checkpointRateModel.getBorrowRate(cash, borrows, reserves)).to.equal(200);
+    expect(await checkpointRateModel.getSupplyRate(cash, borrows, reserves, reserveFactorMantissa)).to.equal(100);
+  });
+
+  it("should return the correct current rate model", async () => {
+    expect(await checkpointRateModel.currentRateModel()).to.equal(oldRateModel.address);
+
+    await time.increaseTo(checkpointTimestamp);
+
+    expect(await checkpointRateModel.currentRateModel()).to.equal(newRateModel.address);
+  });
+});
