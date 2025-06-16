a2b922316a8b2835565439ed9a23f946eef2a0f3
web3rover <arsenal.narayan@gmail.com>
2025-06-16 18:55:11 +0530

    feat: added script to fetch holders and update scores

---
+++ deploy/018-prime-update-scores.ts
@@ -0,0 +1,75 @@
+import { ethers } from "hardhat";
+import { DeployFunction } from "hardhat-deploy/types";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+
+import { Prime } from "../typechain";
+
+interface ScoreUpdate {
+  [key: string]: string[];
+}
+
+const fetchPrimeHolders = async (prime: Prime, fromBlock: number, toBlock: number): Promise<string[]> => {
+  const events = await prime.queryFilter(prime.filters.Mint(), fromBlock, toBlock);
+  const users = [];
+
+  for (const event of events) {
+    const user = event.args[0];
+    users.push(user);
+  }
+
+  return users;
+};
+
+const func: DeployFunction = async function () {
+  const prime: Prime = await ethers.getContract(`Prime`);
+  const primeHolders: string[] = [];
+
+  const fromBlock = 33264762;
+  const toBlock = await ethers.provider.getBlockNumber();
+  const chunkSize = 50000;
+
+  let startBlock = fromBlock;
+
+  while (startBlock <= toBlock) {
+    const endBlock = Math.min(startBlock + chunkSize - 1, toBlock);
+    const users = await fetchPrimeHolders(prime, startBlock, endBlock);
+    if (users.length !== 0) {
+      console.log(`Fetched ${users.length} prime holders from block ${startBlock} to ${endBlock}`);
+    }
+    primeHolders.push(...users);
+
+    console.log(`Fetched events from block ${startBlock} to ${endBlock}`);
+    startBlock = endBlock + 1;
+  }
+
+  const currentPrimeHolders = [];
+
+  // check if user is a prime holder or not
+  for (let i = 0; i < primeHolders.length; i++) {
+    const token = await prime.tokens(primeHolders[i]);
+
+    if (token.exists) {
+      currentPrimeHolders.push(primeHolders[i]);
+    }
+  }
+
+  console.log(`Total Prime Holders: ${currentPrimeHolders.length}`);
+  console.log("Pending score updates: ", await prime.pendingScoreUpdates());
+
+  // update scrores with batch size 100
+  const batchSize = 100;
+  for (let i = 0; i < currentPrimeHolders.length; i += batchSize) {
+    const batch = currentPrimeHolders.slice(i, i + batchSize);
+    const tx = await prime.updateScores(batch);
+    await tx.wait();
+
+    console.log(`Updated scores for ${batch.length} users in batch starting from index ${i}`);
+  }
+
+  console.log("Pending score updates: ", await prime.pendingScoreUpdates());
+};
+
+func.tags = ["prime-update-scores"];
+func.skip = async (hre: HardhatRuntimeEnvironment) => !hre.network.live;
+
+export default func;
