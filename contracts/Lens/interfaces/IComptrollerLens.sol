3b998cac131a08f0ed4e4cc18d715981661ac5da
Kirill Kuvshinov <kkirka@gmail.com>
2025-08-07 19:40:35 +0300

    fixup! docs: add missing natspec to interfaces

--- contracts/Lens/interfaces/IComptrollerLens.sol
+++ contracts/Lens/interfaces/IComptrollerLens.sol
@@ -4,6 +4,14 @@ pragma solidity 0.8.25;
 import { IVToken } from "../../Tokens/VTokens/interfaces/IVToken.sol";
 
 interface IComptrollerLens {
+    /**
+     * @notice Computes the number of collateral tokens to be seized in a liquidation event
+     * @param comptroller Address of comptroller
+     * @param vTokenBorrowed Address of the borrowed vToken
+     * @param vTokenCollateral Address of collateral for the borrow
+     * @param actualRepayAmount Repayment amount i.e amount to be repaid of total borrowed amount
+     * @return A tuple of error code, and tokens to seize
+     */
     function liquidateCalculateSeizeTokens(
         address comptroller,
         address vTokenBorrowed,
@@ -11,12 +19,28 @@ interface IComptrollerLens {
         uint actualRepayAmount
     ) external view returns (uint, uint);
 
+    /**
+     * @notice Computes the number of VAI tokens to be seized in a liquidation event
+     * @param comptroller Address of comptroller
+     * @param vTokenCollateral Address of collateral for vToken
+     * @param actualRepayAmount Repayment amount i.e amount to be repaid of the total borrowed amount
+     * @return A tuple of error code, and tokens to seize
+     */
     function liquidateVAICalculateSeizeTokens(
         address comptroller,
         address vTokenCollateral,
         uint actualRepayAmount
     ) external view returns (uint, uint);
 
+    /**
+     * @notice Determine the current account liquidity wrt collateral requirements
+     * @param comptroller Address of comptroller
+     * @param account The account to determine liquidity for
+     * @param vTokenModify The market to hypothetically modify
+     * @param redeemTokens The number of tokens to hypothetically redeem
+     * @param borrowAmount The amount of underlying to hypothetically borrow
+     * @return A tuple of error code, liquidity, and shortfall
+     */
     function getHypotheticalAccountLiquidity(
         address comptroller,
         address account,
