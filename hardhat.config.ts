a4c9e27ef01064cf2ac8649a36f74c5e6224f9fb
Kirill Kuvshinov <kkirka@gmail.com>
2025-08-07 16:11:50 +0300

    chore: update target EVM version to cancun in hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -158,7 +158,7 @@ const config: HardhatUserConfig = {
             enabled: true,
             runs: 200,
           },
-          evmVersion: "paris",
+          evmVersion: "cancun",
           outputSelection: {
             "*": {
               "*": ["storageLayout"],
