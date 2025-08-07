b1cbdadf4823e0d1007ae88db55da943a1617c4e
Kirill Kuvshinov <kkirka@gmail.com>
2025-08-07 16:04:08 +0300

    fixup! docs: add missing natspec to interfaces

--- contracts/VAIVault/IVAIVault.sol
+++ contracts/VAIVault/IVAIVault.sol
@@ -2,5 +2,9 @@
 pragma solidity 0.8.25;
 
 interface IVAIVault {
+    /**
+     * @notice Updates pending rewards for all users
+     * @dev This function must be called before any operation that changes user balances or reward parameters
+     */
     function updatePendingRewards() external;
 }
