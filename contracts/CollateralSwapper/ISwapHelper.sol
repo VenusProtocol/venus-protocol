9de725c7fd89c50f37b0811f51e82b558e37d540
web3rover <arsenal.narayan@gmail.com>
2025-08-12 12:11:13 +0530

    fix: removed collateral swapper

--- contracts/CollateralSwapper/ISwapHelper.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: BSD-3-Clause
-pragma solidity 0.8.25;
-
-interface ISwapHelper {
-    function swapInternal(address tokenA, address tokenB, uint256 amount) external payable;
-}
