24b58bc883c48ac990c218d6d6522c1b2b0262f3
web3rover <arsenal.narayan@gmail.com>
2025-08-19 21:30:13 +0530

    fix: vcc-07

--- contracts/Tokens/VTokens/VToken.sol
+++ contracts/Tokens/VTokens/VToken.sol
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: BSD-3-Clause
+
 pragma solidity 0.8.25;
 
 import { IAccessControlManagerV8 } from "@venusprotocol/governance-contracts/contracts/Governance/IAccessControlManagerV8.sol";
