3cacf4606313e7fecf73a6db1ae60a414a85e773
Kirill Kuvshinov <kkirka@gmail.com>
2025-09-08 18:03:00 +0300

    fix(legacy): add storage gap to legacy VTokenStorageR1

--- contracts/Tokens/VTokens/legacy/VTokenStorageR1.sol
+++ contracts/Tokens/VTokens/legacy/VTokenStorageR1.sol
@@ -143,6 +143,12 @@ contract VTokenStorageR1 {
     /**
      * @notice address of accessControlManager
      */
-
     address public accessControlManager;
+
+    /**
+     * @dev This empty reserved space is put in place to allow future versions to add new
+     * variables without shifting down storage in the inheritance chain.
+     * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
+     */
+    uint256[50] private __gap;
 }
