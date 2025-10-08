a1311c5c73cf946206804d8bfddaae29d64654d5
GitGuru7 <aditya@venus.io>
2025-10-08 19:55:12 +0530

    fix: add missing natSpec

--- contracts/Comptroller/Diamond/facets/FlashLoanFacet.sol
+++ contracts/Comptroller/Diamond/facets/FlashLoanFacet.sol
@@ -33,6 +33,7 @@ contract FlashLoanFacet is IFlashLoanFacet, FacetBase, ReentrancyGuardTransient 
      * @custom:error InvalidAmount is thrown if the requested amount is zero.
      * @custom:error NoAssetsRequested is thrown if no assets are requested for the flash loan.
      * @custom:error InvalidFlashLoanParams is thrown if the flash loan params are invalid.
+     * @custom:error MarketNotListed is thrown if the specified vToken market is not listed.
      * @custom:error SenderNotAuthorizedForFlashLoan is thrown if the sender is not authorized to use flashloan.
      * @custom:error NotAnApprovedDelegate is thrown if `msg.sender` is not `onBehalf` or an approved delegate for `onBehalf`.
      * @custom:event Emits FlashLoanExecuted on success
