d546c0f20465384ad8f51eb88a03acae513bbe10
Debugger022 <debugger022@gmail.com>
2025-12-03 12:31:31 +0530

    fix: update blocks per day in VenusLens

--- contracts/Lens/VenusLens.sol
+++ contracts/Lens/VenusLens.sol
@@ -12,7 +12,7 @@ import { IXVS } from "../Tokens/XVS/IXVS.sol";
 
 contract VenusLens is ExponentialNoError {
     /// @notice Blocks Per Day
-    uint public constant BLOCKS_PER_DAY = 115200;
+    uint public constant BLOCKS_PER_DAY = 192000;
 
     /// @notice Total actions available on VToken
     uint public constant VTOKEN_ACTIONS = 8;
