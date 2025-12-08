f9c3d5586346df8508bfd76bb7713a630cbeaebc
Debugger022 <debugger022@gmail.com>
2025-12-08 12:39:37 +0530

    fix: test

--- tests/hardhat/Fork/TokenRedeemer.ts
+++ tests/hardhat/Fork/TokenRedeemer.ts
@@ -752,22 +752,22 @@ const test = (setup: () => Promise<TokenRedeemerFixture>) => () => {
         await vai.mint(redeemer.address, 3);
         expect(await vai.balanceOf(redeemer.address)).to.equal(3);
         await ethers.provider.send("evm_setAutomine", [false]);
-        await vaiController.setBaseRate(parseUnits("420480", 18)); // 1% each block
+        await vaiController.setBaseRate(parseUnits("700800", 18)); // 1% each block
         await mine(99);
         await vaiController.accrueVAIInterest();
         await mine();
-        // 100 blocks here, so debt before the repayment is twice the initial amount
+        // 100 blocks here, with higher BaseRate debt grows more than twice the initial amount
         expect(await vaiController.getVAIRepayAmount(borrowers[0].address)).to.equal(2);
-        expect(await vaiController.getVAIRepayAmount(borrowers[1].address)).to.equal(4);
-        expect(await vaiController.getVAIRepayAmount(borrowers[2].address)).to.equal(6);
+        expect(await vaiController.getVAIRepayAmount(borrowers[1].address)).to.equal(5);
+        expect(await vaiController.getVAIRepayAmount(borrowers[2].address)).to.equal(8);
         // We transfer the refund to someone instead of treasury here so that we don't need
         // to account for interest that is also transferred to treasury
         const tx = await redeemer.connect(owner).batchRepayVAI(vaiController.address, repayments, someone.address);
         await mine();
         expect(await vaiController.getVAIRepayAmount(borrowers[0].address)).to.equal(0);
         // The second repayment doesn't happen due to rounding in VAIController
-        expect(await vaiController.getVAIRepayAmount(borrowers[1].address)).to.equal(4);
-        expect(await vaiController.getVAIRepayAmount(borrowers[2].address)).to.equal(6);
+        expect(await vaiController.getVAIRepayAmount(borrowers[1].address)).to.equal(5);
+        expect(await vaiController.getVAIRepayAmount(borrowers[2].address)).to.equal(8);
         await ethers.provider.send("evm_setAutomine", [true]);
         // Still transfers 1 wei refund to treasury
         await expect(tx).to.changeTokenBalance(vai, someone.address, 1);
